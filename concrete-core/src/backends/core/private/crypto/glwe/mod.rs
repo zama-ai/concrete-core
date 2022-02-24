037db5bd4d789ee8a6be21122211bfb0a620bcf0
orelcosseron <orel.cosseron@zama.ai>
2022-02-24 15:07:18 +0100

    feat(concrete_core): Adds GlweSeededCiphertext

--- concrete-core/src/backends/core/private/crypto/glwe/mod.rs
+++ concrete-core/src/backends/core/private/crypto/glwe/mod.rs
@@ -2,6 +2,7 @@
 
 pub use body::*;
 pub use ciphertext::*;
+pub use ciphertext_seeded::*;
 pub use list::*;
 pub use mask::*;
 
@@ -10,5 +11,6 @@ mod tests;
 
 mod body;
 mod ciphertext;
+mod ciphertext_seeded;
 mod list;
 mod mask;
--- concrete-core/src/backends/core/private/crypto/glwe/tests.rs
+++ concrete-core/src/backends/core/private/crypto/glwe/tests.rs
@@ -10,6 +10,8 @@ use crate::backends::core::private::test_tools;
 use crate::backends::core::private::test_tools::assert_delta_std_dev;
 use concrete_commons::dispersion::LogStandardDev;
 
+use super::{GlweCiphertext, GlweSeededCiphertext};
+
 fn test_glwe<T: UnsignedTorus>() {
     // random settings
     let nb_ct = test_tools::random_ciphertext_count(200);
@@ -55,4 +57,45 @@ fn test_glwe_encrypt_decrypt_u32() {
 #[test]
 fn test_glwe_encrypt_decrypt_u64() {
     test_glwe::<u64>();
+}
+
+fn test_seeded_glwe<T: UnsignedTorus>() {
+    // random settings
+    let dimension = test_tools::random_glwe_dimension(5);
+    let polynomial_size = test_tools::random_polynomial_size(200);
+    let noise_parameter = LogStandardDev::from_log_standard_dev(-20.);
+    let mut generator = SecretRandomGenerator::new(None);
+
+    // generates a secret key
+    let sk = GlweSecretKey::generate_binary(dimension, polynomial_size, &mut generator);
+
+    // generates random plaintexts
+    let plaintext = PlaintextList::from_tensor(generator.random_uniform_tensor(polynomial_size.0));
+
+    // encrypts
+    let mut ciphertext = GlweSeededCiphertext::allocate(T::ZERO, polynomial_size, dimension);
+    sk.encrypt_seeded_glwe(&mut ciphertext, &plaintext, noise_parameter);
+
+    // expands
+    let mut ciphertext_expanded =
+        GlweCiphertext::allocate(T::ZERO, polynomial_size, dimension.to_glwe_size());
+    ciphertext.expand_into(&mut ciphertext_expanded);
+
+    // decrypts
+    let mut decryptions =
+        PlaintextList::from_tensor(generator.random_uniform_tensor(polynomial_size.0));
+    sk.decrypt_glwe(&mut decryptions, &ciphertext_expanded);
+
+    // test
+    assert_delta_std_dev(&plaintext, &decryptions, noise_parameter);
+}
+
+#[test]
+fn test_seeded_glwe_u32() {
+    test_seeded_glwe::<u32>();
+}
+
+#[test]
+fn test_seeded_glwe_u64() {
+    test_seeded_glwe::<u64>();
 }
