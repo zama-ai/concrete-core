24d08ddbd57590f538193112f357cfe0e544b677
Ben <ben.curtis@zama.ai>
2022-03-07 12:37:38 +0000

    feat(core): add default-backend utilities for tensor product

--- concrete-core/src/backends/default/implementation/entities/glwe_secret_key.rs
+++ concrete-core/src/backends/default/implementation/entities/glwe_secret_key.rs
@@ -5,6 +5,8 @@ use concrete_commons::key_kinds::BinaryKeyKind;
 use concrete_commons::parameters::{GlweDimension, PolynomialSize};
 #[cfg(feature = "serde_serialize")]
 use serde::{Deserialize, Serialize};
+use crate::prelude::markers::TensorProductKeyDistribution;
+use crate::prelude::TensorProductKeyKind;
 
 /// A structure representing a GLWE secret key with 32 bits of precision.
 #[cfg_attr(feature = "serde_serialize", derive(Serialize, Deserialize))]
@@ -34,6 +36,45 @@ impl AbstractEntity for GlweSecretKey64 {
 }
 impl GlweSecretKeyEntity for GlweSecretKey64 {
     type KeyDistribution = BinaryKeyDistribution;
+
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.key_size()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+}
+
+/// A structure representing a GLWE tensored secret key with 32 bits of precision.
+#[cfg_attr(feature = "serde_serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct GlweTensorProductSecretKey32(pub(crate) ImpGlweSecretKey<TensorProductKeyKind, 
+    Vec<u32>>);
+impl AbstractEntity for GlweTensorProductSecretKey32 {
+    type Kind = GlweSecretKeyKind;
+}
+impl GlweSecretKeyEntity for GlweTensorProductSecretKey32 {
+    type KeyDistribution = TensorProductKeyDistribution;
+
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.key_size()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+}
+
+/// A structure representing a GLWE tensor product secret key with 64 bits of precision.
+#[cfg_attr(feature = "serde_serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct GlweTensorProductSecretKey64(pub(crate) ImpGlweSecretKey<TensorProductKeyKind, Vec<u64>>);
+impl AbstractEntity for GlweTensorProductSecretKey64 {
+    type Kind = GlweSecretKeyKind;
+}
+impl GlweSecretKeyEntity for GlweTensorProductSecretKey64 {
+    type KeyDistribution = TensorProductKeyDistribution;
 
     fn glwe_dimension(&self) -> GlweDimension {
         self.0.key_size()
