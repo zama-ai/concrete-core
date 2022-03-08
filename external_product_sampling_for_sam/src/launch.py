f748fe64e4c6c9bf373d126d3a52e0cb4714c5b1
Samuel Tap <samuel.tap@cryptoexperts.com>
2022-03-08 17:13:50 +0100

    add command for bash

--- external_product_sampling_for_sam/src/launch.py
+++ external_product_sampling_for_sam/src/launch.py
@@ -2,6 +2,7 @@
 import subprocess
 
 bashCommand = "RUSTFLAGS=\"-Ctarget-cpu=native\" cargo run --release -- --poly-size {}"
+scp_bashcomand = "scp -i gpu_tests.pem ubuntu@ec2-35-180-134-133.eu-west-3.compute.amazonaws.com:/home/ubuntu/concrete/external_product_sampling_for_sam/src/2048.acquisition_external_product_k=1 2048.acquisition_external_product_k=1"
 # bashCommand = "cargo run --release -- --poly-size {}"
 
 N_MIN = 8
@@ -11,3 +12,6 @@ for N in poly_sizes:
     cmd = bashCommand.format(N)
     print(cmd)
     process = subprocess.Popen(cmd, shell=True)
+
+
+
