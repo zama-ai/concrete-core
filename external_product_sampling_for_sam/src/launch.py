1ef0aa0a44f16571414fba4f0ee3d93dee1158f3
Samuel Tap <samuel.tap@zama.ai>
2022-03-24 16:18:19 +0100

    new launch script

--- external_product_sampling_for_sam/src/launch.py
+++ external_product_sampling_for_sam/src/launch.py
@@ -1,15 +1,14 @@
 
 import subprocess
 
-bashCommand = "RUSTFLAGS=\"-Ctarget-cpu=native\" cargo run --release -- --poly-size {}"
+bashCommand = "RUSTFLAGS=\"-Ctarget-cpu=native\" cargo run --release -- --tot {} --id {}"
 scp_bashcomand = "scp -i gpu_tests.pem ubuntu@ec2-35-180-134-133.eu-west-3.compute.amazonaws.com:/home/ubuntu/concrete/external_product_sampling_for_sam/src/2048.acquisition_external_product_k=1 2048.acquisition_external_product_k=1"
 # bashCommand = "cargo run --release -- --poly-size {}"
 
-N_MIN = 8
-N_MAX = 10 #14 + 1
-poly_sizes= [2**N for N in range(N_MIN, N_MAX)]
-for N in poly_sizes:
-    cmd = bashCommand.format(N)
+tot=10
+
+for id in range(tot):
+    cmd = bashCommand.format(tot, id)
     print(cmd)
     process = subprocess.Popen(cmd, shell=True)
 
