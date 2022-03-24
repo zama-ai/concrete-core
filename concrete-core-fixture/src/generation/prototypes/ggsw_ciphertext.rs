1e90e2b81d8b50328307ebee8a988a6b98072f24
Agnes Leroy <agnes.leroy@zama.ai>
2022-03-24 13:42:14 +0100

    Change the repetitions to have directly the GGSW in the Fourier domain

--- concrete-core-fixture/src/generation/prototypes/ggsw_ciphertext.rs
+++ concrete-core-fixture/src/generation/prototypes/ggsw_ciphertext.rs
@@ -1,6 +1,8 @@
 use crate::generation::{IntegerPrecision, Precision32, Precision64};
 use concrete_core::prelude::markers::{BinaryKeyDistribution, KeyDistributionMarker};
-use concrete_core::prelude::{GgswCiphertext32, GgswCiphertext64};
+use concrete_core::prelude::{
+    FourierGgswCiphertext32, FourierGgswCiphertext64, GgswCiphertext32, GgswCiphertext64,
+};
 
 /// A trait implemented by ggsw ciphertext prototypes.
 pub trait GgswCiphertextPrototype {
@@ -15,9 +17,25 @@ impl GgswCiphertextPrototype for ProtoBinaryGgswCiphertext32 {
     type Precision = Precision32;
 }
 
-/// A type representing the prototype of a 64 bit binary glwe ciphertext entity.
+/// A type representing the prototype of a 64 bit binary ggsw ciphertext entity.
 pub struct ProtoBinaryGgswCiphertext64(pub(crate) GgswCiphertext64);
 impl GgswCiphertextPrototype for ProtoBinaryGgswCiphertext64 {
+    type KeyDistribution = BinaryKeyDistribution;
+    type Precision = Precision64;
+}
+
+/// A type representing the prototype of a 32 bit binary ggsw ciphertext entity
+/// in the Fourier domain.
+pub struct ProtoBinaryFourierGgswCiphertext32(pub(crate) FourierGgswCiphertext32);
+impl GgswCiphertextPrototype for ProtoBinaryFourierGgswCiphertext32 {
+    type KeyDistribution = BinaryKeyDistribution;
+    type Precision = Precision32;
+}
+
+/// A type representing the prototype of a 64 bit binary ggsw ciphertext entity
+/// in the Fourier domain.
+pub struct ProtoBinaryFourierGgswCiphertext64(pub(crate) FourierGgswCiphertext64);
+impl GgswCiphertextPrototype for ProtoBinaryFourierGgswCiphertext64 {
     type KeyDistribution = BinaryKeyDistribution;
     type Precision = Precision64;
 }
--- concrete-core-fixture/src/generation/synthesizing/ggsw_ciphertext.rs
+++ concrete-core-fixture/src/generation/synthesizing/ggsw_ciphertext.rs
@@ -21,70 +21,30 @@ where
 
 #[cfg(feature = "backend_core")]
 mod backend_core {
-    use crate::generation::prototypes::{ProtoBinaryGgswCiphertext32, ProtoBinaryGgswCiphertext64};
+    use crate::generation::prototypes::{
+        ProtoBinaryFourierGgswCiphertext32, ProtoBinaryFourierGgswCiphertext64,
+        ProtoBinaryGgswCiphertext32, ProtoBinaryGgswCiphertext64,
+    };
     use crate::generation::synthesizing::SynthesizesGgswCiphertext;
     use crate::generation::{Maker, Precision32, Precision64};
     use concrete_core::prelude::{
         DestructionEngine, FourierGgswCiphertext32, FourierGgswCiphertext64, GgswCiphertext32,
-        GgswCiphertext64, GgswCiphertextConversionEngine,
+        GgswCiphertext64,
     };
-
-    impl SynthesizesGgswCiphertext<Precision32, GgswCiphertext32> for Maker {
-        fn synthesize_ggsw_ciphertext(
-            &mut self,
-            prototype: &Self::GgswCiphertextProto,
-        ) -> GgswCiphertext32 {
-            prototype.0.to_owned()
-        }
-
-        fn unsynthesize_ggsw_ciphertext(
-            &mut self,
-            entity: &GgswCiphertext32,
-        ) -> Self::GgswCiphertextProto {
-            ProtoBinaryGgswCiphertext32(entity.to_owned())
-        }
-
-        fn destroy_ggsw_ciphertext(&mut self, entity: GgswCiphertext32) {
-            self.core_engine.destroy(entity).unwrap();
-        }
-    }
-
-    impl SynthesizesGgswCiphertext<Precision64, GgswCiphertext64> for Maker {
-        fn synthesize_ggsw_ciphertext(
-            &mut self,
-            prototype: &Self::GgswCiphertextProto,
-        ) -> GgswCiphertext64 {
-            prototype.0.to_owned()
-        }
-
-        fn unsynthesize_ggsw_ciphertext(
-            &mut self,
-            entity: &GgswCiphertext64,
-        ) -> Self::GgswCiphertextProto {
-            ProtoBinaryGgswCiphertext64(entity.to_owned())
-        }
-
-        fn destroy_ggsw_ciphertext(&mut self, entity: GgswCiphertext64) {
-            self.core_engine.destroy(entity).unwrap();
-        }
-    }
 
     impl SynthesizesGgswCiphertext<Precision32, FourierGgswCiphertext32> for Maker {
         fn synthesize_ggsw_ciphertext(
             &mut self,
             prototype: &Self::GgswCiphertextProto,
         ) -> FourierGgswCiphertext32 {
-            self.core_engine
-                .convert_ggsw_ciphertext(&prototype.0)
-                .unwrap()
+            prototype.0.to_owned()
         }
 
         fn unsynthesize_ggsw_ciphertext(
             &mut self,
-            _entity: &FourierGgswCiphertext32,
+            entity: &FourierGgswCiphertext32,
         ) -> Self::GgswCiphertextProto {
-            // FIXME:
-            unimplemented!("The backward fourier conversion was not yet implemented");
+            ProtoBinaryFourierGgswCiphertext32(entity.to_owned())
         }
 
         fn destroy_ggsw_ciphertext(&mut self, entity: FourierGgswCiphertext32) {
@@ -97,17 +57,14 @@ mod backend_core {
             &mut self,
             prototype: &Self::GgswCiphertextProto,
         ) -> FourierGgswCiphertext64 {
-            self.core_engine
-                .convert_ggsw_ciphertext(&prototype.0)
-                .unwrap()
+            prototype.0.to_owned()
         }
 
         fn unsynthesize_ggsw_ciphertext(
             &mut self,
-            _entity: &FourierGgswCiphertext64,
+            entity: &FourierGgswCiphertext64,
         ) -> Self::GgswCiphertextProto {
-            // FIXME:
-            unimplemented!("The backward fourier conversion was not yet implemented");
+            ProtoBinaryFourierGgswCiphertext64(entity.to_owned())
         }
 
         fn destroy_ggsw_ciphertext(&mut self, entity: FourierGgswCiphertext64) {
