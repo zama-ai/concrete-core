0fcaef7bb1df9b6abd3f0d1927a24a35d4ff9a89
Samuel Tap <samuel.tap@zama.ai>
2022-03-24 16:49:10 +0100

    right thread nb

--- external_product_sampling_for_sam/src/launch.py
+++ external_product_sampling_for_sam/src/launch.py
@@ -1,16 +1,10 @@
-
 import subprocess
 
-bashCommand = "RUSTFLAGS=\"-Ctarget-cpu=native\" cargo run --release -- --tot {} --id {}"
-scp_bashcomand = "scp -i gpu_tests.pem ubuntu@ec2-35-180-134-133.eu-west-3.compute.amazonaws.com:/home/ubuntu/concrete/external_product_sampling_for_sam/src/2048.acquisition_external_product_k=1 2048.acquisition_external_product_k=1"
-# bashCommand = "cargo run --release -- --poly-size {}"
-
-tot=10
+bashCommand = "RUSTFLAGS=\"-C target-cpu=native\" cargo run --release -- --tot {} --id {}"
+tot = 7
 
 for id in range(tot):
     cmd = bashCommand.format(tot, id)
     print(cmd)
     process = subprocess.Popen(cmd, shell=True)
-
-
 
