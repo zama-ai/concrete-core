31b2580bf077591da8afbcfbfaffdb6adbcf5462
Arthur Meyre <arthur.meyre@zama.ai>
2022-04-14 15:02:38 +0200

    chore(xtask): add an xtask to automatically escape _ in latex equations

--- concrete-tasks/src/chore.rs
+++ concrete-tasks/src/chore.rs
@@ -1,6 +1,10 @@
-use crate::cmd;
+use crate::{cmd, format_latex_doc};
 use std::io::Error;
 
 pub fn format() -> Result<(), Error> {
     cmd!("cargo +nightly fmt")
+}
+
+pub fn format_latex_doc() -> Result<(), Error> {
+    format_latex_doc::escape_underscore_in_latex_doc()
 }
