6db1ca64c420043e5cdb112860a6b7fbc543ea92
Florent Michel <florent.michel@optalysys.com>
2022-06-01 10:23:25 +0100

    remove the need to iimplement Eq for the LweSecretKey s

--- concrete-core/src/backends/default/implementation/entities/lwe_secret_key.rs
+++ concrete-core/src/backends/default/implementation/entities/lwe_secret_key.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 
 /// A structure representing an LWE secret key with 32 bits of precision.
 #[cfg_attr(feature = "serde_serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct LweSecretKey32(pub(crate) ImpLweSecretKey<BinaryKeyKind, Vec<u32>>);
 impl AbstractEntity for LweSecretKey32 {
     type Kind = LweSecretKeyKind;
@@ -23,7 +23,7 @@ impl LweSecretKeyEntity for LweSecretKey32 {
 
 /// A structure representing an LWE secret key with 64 bits of precision.
 #[cfg_attr(feature = "serde_serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct LweSecretKey64(pub(crate) ImpLweSecretKey<BinaryKeyKind, Vec<u64>>);
 impl AbstractEntity for LweSecretKey64 {
     type Kind = LweSecretKeyKind;
