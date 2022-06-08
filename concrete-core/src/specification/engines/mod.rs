learningspoons-go/concrete

feat(core): add a spec for the tensor product

concrete-core/src/specification/engines/mod.rs
@@ -159,9 +159,11 @@ mod glwe_ciphertext_decryption;
 mod glwe_ciphertext_discarding_conversion;
 mod glwe_ciphertext_discarding_decryption;
 mod glwe_ciphertext_discarding_encryption;
+mod glwe_ciphertext_discarding_tensor_product_computation;
 mod glwe_ciphertext_encryption;
 mod glwe_ciphertext_ggsw_ciphertext_discarding_external_product;
 mod glwe_ciphertext_ggsw_ciphertext_external_product;
+mod glwe_ciphertext_tensor_product_computation;
 mod glwe_ciphertext_trivial_decryption;
 mod glwe_ciphertext_trivial_encryption;
 mod glwe_ciphertext_vector_conversion;
@@ -177,6 +179,8 @@ mod glwe_ciphertext_zero_encryption;
 mod glwe_secret_key_conversion;
 mod glwe_secret_key_creation;
 mod glwe_secret_key_discarding_conversion;
+mod glwe_secret_key_discarding_tensor_product;
+mod glwe_secret_key_tensor_product;
 mod glwe_secret_key_to_lwe_secret_key_transmutation;
 mod lwe_bootstrap_key_conversion;
 mod lwe_bootstrap_key_creation;
@@ -270,9 +274,11 @@ pub use glwe_ciphertext_decryption::*;
 pub use glwe_ciphertext_discarding_conversion::*;
 pub use glwe_ciphertext_discarding_decryption::*;
 pub use glwe_ciphertext_discarding_encryption::*;
+pub use glwe_ciphertext_discarding_tensor_product_computation::*;
 pub use glwe_ciphertext_encryption::*;
 pub use glwe_ciphertext_ggsw_ciphertext_discarding_external_product::*;
 pub use glwe_ciphertext_ggsw_ciphertext_external_product::*;
+pub use glwe_ciphertext_tensor_product_computation::*;
 pub use glwe_ciphertext_trivial_decryption::*;
 pub use glwe_ciphertext_trivial_encryption::*;
 pub use glwe_ciphertext_vector_conversion::*;
@@ -288,6 +294,8 @@ pub use glwe_ciphertext_zero_encryption::*;
 pub use glwe_secret_key_conversion::*;
 pub use glwe_secret_key_creation::*;
 pub use glwe_secret_key_discarding_conversion::*;
+pub use glwe_secret_key_discarding_tensor_product::*;
+pub use glwe_secret_key_tensor_product::*;
 pub use glwe_secret_key_to_lwe_secret_key_transmutation::*;
 pub use lwe_bootstrap_key_conversion::*;
 pub use lwe_bootstrap_key_creation::*;

zama-ai/concrete-core

concrete-core/src/specification/engines/mod.rs
@@ -164,10 +164,12 @@ mod glwe_ciphertext_decryption;
 mod glwe_ciphertext_discarding_conversion;
 mod glwe_ciphertext_discarding_decryption;
 mod glwe_ciphertext_discarding_encryption;
+mod glwe_ciphertext_discarding_tensor_product_same_key;
 mod glwe_ciphertext_discarding_trivial_encryption;
 mod glwe_ciphertext_encryption;
 mod glwe_ciphertext_ggsw_ciphertext_discarding_external_product;
 mod glwe_ciphertext_ggsw_ciphertext_external_product;
+mod glwe_ciphertext_tensor_product_same_key;
 mod glwe_ciphertext_trivial_decryption;
 mod glwe_ciphertext_trivial_encryption;
 mod glwe_ciphertext_vector_conversion;
@@ -183,6 +185,8 @@ mod glwe_ciphertext_zero_encryption;
 mod glwe_secret_key_conversion;
 mod glwe_secret_key_creation;
 mod glwe_secret_key_discarding_conversion;
+mod glwe_secret_key_discarding_tensor_product_same_key;
+mod glwe_secret_key_tensor_product_same_key;
 mod glwe_to_lwe_secret_key_transmutation;
 mod lwe_bootstrap_key_conversion;
 mod lwe_bootstrap_key_creation;
@@ -283,10 +287,12 @@ pub use glwe_ciphertext_decryption::*;
 pub use glwe_ciphertext_discarding_conversion::*;
 pub use glwe_ciphertext_discarding_decryption::*;
 pub use glwe_ciphertext_discarding_encryption::*;
+pub use glwe_ciphertext_discarding_tensor_product_same_key::*;
 pub use glwe_ciphertext_discarding_trivial_encryption::*;
 pub use glwe_ciphertext_encryption::*;
 pub use glwe_ciphertext_ggsw_ciphertext_discarding_external_product::*;
 pub use glwe_ciphertext_ggsw_ciphertext_external_product::*;
+pub use glwe_ciphertext_tensor_product_same_key::*;
 pub use glwe_ciphertext_trivial_decryption::*;
 pub use glwe_ciphertext_trivial_encryption::*;
 pub use glwe_ciphertext_vector_conversion::*;
@@ -302,6 +308,8 @@ pub use glwe_ciphertext_zero_encryption::*;
 pub use glwe_secret_key_conversion::*;
 pub use glwe_secret_key_creation::*;
 pub use glwe_secret_key_discarding_conversion::*;
+pub use glwe_secret_key_discarding_tensor_product_same_key::*;
+pub use glwe_secret_key_tensor_product_same_key::*;
 pub use glwe_to_lwe_secret_key_transmutation::*;
 pub use lwe_bootstrap_key_conversion::*;
 pub use lwe_bootstrap_key_creation::*;