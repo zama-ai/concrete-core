9d88fc54924d26f1ae2c3492fd73ded666823206
Arthur Meyre <arthur.meyre@zama.ai>
2022-06-24 10:56:31 +0200

    feat(fixture): add prototyping utilities for LweSeededCiphertextVector

---
+++ concrete-core-fixture/src/generation/synthesizing/lwe_seeded_ciphertext_vector.rs
@@ -0,0 +1,73 @@
+use crate::generation::prototyping::PrototypesLweSeededCiphertextVector;
+use crate::generation::IntegerPrecision;
+use concrete_core::prelude::LweSeededCiphertextVectorEntity;
+
+/// A trait allowing to synthesize an actual LweSeededCiphertextVectorEntity from a prototype.
+pub trait SynthesizesLweSeededCiphertextVector<
+    Precision: IntegerPrecision,
+    LweSeededCiphertextVector,
+>:
+    PrototypesLweSeededCiphertextVector<Precision, LweSeededCiphertextVector::KeyDistribution> where
+    LweSeededCiphertextVector: LweSeededCiphertextVectorEntity,
+{
+    fn synthesize_lwe_seeded_ciphertext_vector(
+        &mut self,
+        prototype: &Self::LweSeededCiphertextVectorProto,
+    ) -> LweSeededCiphertextVector;
+    fn unsynthesize_lwe_seeded_ciphertext_vector(
+        &mut self,
+        entity: LweSeededCiphertextVector,
+    ) -> Self::LweSeededCiphertextVectorProto;
+    fn destroy_lwe_seeded_ciphertext_vector(&mut self, entity: LweSeededCiphertextVector);
+}
+
+mod backend_default {
+    use crate::generation::prototypes::{
+        ProtoBinaryLweSeededCiphertextVector32, ProtoBinaryLweSeededCiphertextVector64,
+    };
+    use crate::generation::synthesizing::SynthesizesLweSeededCiphertextVector;
+    use crate::generation::{Maker, Precision32, Precision64};
+    use concrete_core::prelude::{
+        DestructionEngine, LweSeededCiphertextVector32, LweSeededCiphertextVector64,
+    };
+
+    impl SynthesizesLweSeededCiphertextVector<Precision32, LweSeededCiphertextVector32> for Maker {
+        fn synthesize_lwe_seeded_ciphertext_vector(
+            &mut self,
+            prototype: &Self::LweSeededCiphertextVectorProto,
+        ) -> LweSeededCiphertextVector32 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_lwe_seeded_ciphertext_vector(
+            &mut self,
+            entity: LweSeededCiphertextVector32,
+        ) -> Self::LweSeededCiphertextVectorProto {
+            ProtoBinaryLweSeededCiphertextVector32(entity)
+        }
+
+        fn destroy_lwe_seeded_ciphertext_vector(&mut self, entity: LweSeededCiphertextVector32) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+
+    impl SynthesizesLweSeededCiphertextVector<Precision64, LweSeededCiphertextVector64> for Maker {
+        fn synthesize_lwe_seeded_ciphertext_vector(
+            &mut self,
+            prototype: &Self::LweSeededCiphertextVectorProto,
+        ) -> LweSeededCiphertextVector64 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_lwe_seeded_ciphertext_vector(
+            &mut self,
+            entity: LweSeededCiphertextVector64,
+        ) -> Self::LweSeededCiphertextVectorProto {
+            ProtoBinaryLweSeededCiphertextVector64(entity)
+        }
+
+        fn destroy_lwe_seeded_ciphertext_vector(&mut self, entity: LweSeededCiphertextVector64) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+}
