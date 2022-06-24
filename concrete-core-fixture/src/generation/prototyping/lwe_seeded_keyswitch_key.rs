8f8d5ad09fbd2b6d7fd7ca7e3ca194a7d091e684
Arthur Meyre <arthur.meyre@zama.ai>
2022-06-24 17:24:55 +0200

    feat(fixture): add prototyping utilities for LweSeededKeyswitchKey

---
+++ concrete-core-fixture/src/generation/prototyping/lwe_seeded_keyswitch_key.rs
@@ -0,0 +1,88 @@
+use crate::generation::prototypes::{
+    LweSeededKeyswitchKeyPrototype, ProtoBinaryBinaryLweSeededKeyswitchKey32,
+    ProtoBinaryBinaryLweSeededKeyswitchKey64,
+};
+use crate::generation::prototyping::lwe_secret_key::PrototypesLweSecretKey;
+use crate::generation::{IntegerPrecision, Maker, Precision32, Precision64};
+use concrete_commons::dispersion::Variance;
+use concrete_commons::parameters::{DecompositionBaseLog, DecompositionLevelCount};
+use concrete_core::prelude::markers::{BinaryKeyDistribution, KeyDistributionMarker};
+use concrete_core::prelude::LweSeededKeyswitchKeyCreationEngine;
+
+/// A trait allowing to manipulate lwe keyswitch key prototypes.
+pub trait PrototypesLweSeededKeyswitchKey<
+    Precision: IntegerPrecision,
+    InputKeyDistribution: KeyDistributionMarker,
+    OutputKeyDistribution: KeyDistributionMarker,
+>:
+    PrototypesLweSecretKey<Precision, InputKeyDistribution>
+    + PrototypesLweSecretKey<Precision, OutputKeyDistribution>
+{
+    type LweSeededKeyswitchKeyProto: LweSeededKeyswitchKeyPrototype<
+        Precision = Precision,
+        InputKeyDistribution = InputKeyDistribution,
+        OutputKeyDistribution = OutputKeyDistribution,
+    >;
+    fn new_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &<Self as PrototypesLweSecretKey<Precision, InputKeyDistribution>>::LweSecretKeyProto,
+        output_key: &<Self as PrototypesLweSecretKey<Precision, OutputKeyDistribution>>::LweSecretKeyProto,
+        decomposition_level: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Self::LweSeededKeyswitchKeyProto;
+}
+
+impl PrototypesLweSeededKeyswitchKey<Precision32, BinaryKeyDistribution, BinaryKeyDistribution>
+    for Maker
+{
+    type LweSeededKeyswitchKeyProto = ProtoBinaryBinaryLweSeededKeyswitchKey32;
+
+    fn new_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &<Self as PrototypesLweSecretKey<Precision32, BinaryKeyDistribution>>::LweSecretKeyProto,
+        output_key: &<Self as PrototypesLweSecretKey<Precision32, BinaryKeyDistribution>>::LweSecretKeyProto,
+        decomposition_level: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Self::LweSeededKeyswitchKeyProto {
+        ProtoBinaryBinaryLweSeededKeyswitchKey32(
+            self.default_engine
+                .create_lwe_seeded_keyswitch_key(
+                    &input_key.0,
+                    &output_key.0,
+                    decomposition_level,
+                    decomposition_base_log,
+                    noise,
+                )
+                .unwrap(),
+        )
+    }
+}
+
+impl PrototypesLweSeededKeyswitchKey<Precision64, BinaryKeyDistribution, BinaryKeyDistribution>
+    for Maker
+{
+    type LweSeededKeyswitchKeyProto = ProtoBinaryBinaryLweSeededKeyswitchKey64;
+
+    fn new_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &<Self as PrototypesLweSecretKey<Precision64, BinaryKeyDistribution>>::LweSecretKeyProto,
+        output_key: &<Self as PrototypesLweSecretKey<Precision64, BinaryKeyDistribution>>::LweSecretKeyProto,
+        decomposition_level: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Self::LweSeededKeyswitchKeyProto {
+        ProtoBinaryBinaryLweSeededKeyswitchKey64(
+            self.default_engine
+                .create_lwe_seeded_keyswitch_key(
+                    &input_key.0,
+                    &output_key.0,
+                    decomposition_level,
+                    decomposition_base_log,
+                    noise,
+                )
+                .unwrap(),
+        )
+    }
+}
---
+++ concrete-core-fixture/src/generation/synthesizing/lwe_seeded_keyswitch_key.rs
@@ -0,0 +1,74 @@
+use crate::generation::prototyping::PrototypesLweSeededKeyswitchKey;
+use crate::generation::IntegerPrecision;
+use concrete_core::prelude::LweSeededKeyswitchKeyEntity;
+
+pub trait SynthesizesLweSeededKeyswitchKey<Precision: IntegerPrecision, LweSeededKeyswitchKey>:
+    PrototypesLweSeededKeyswitchKey<
+    Precision,
+    LweSeededKeyswitchKey::InputKeyDistribution,
+    LweSeededKeyswitchKey::OutputKeyDistribution,
+>
+where
+    LweSeededKeyswitchKey: LweSeededKeyswitchKeyEntity,
+{
+    fn synthesize_lwe_seeded_keyswitch_key(
+        &mut self,
+        prototype: &Self::LweSeededKeyswitchKeyProto,
+    ) -> LweSeededKeyswitchKey;
+    fn unsynthesize_lwe_seeded_keyswitch_key(
+        &mut self,
+        entity: LweSeededKeyswitchKey,
+    ) -> Self::LweSeededKeyswitchKeyProto;
+    fn destroy_lwe_seeded_keyswitch_key(&mut self, entity: LweSeededKeyswitchKey);
+}
+
+mod backend_default {
+    use crate::generation::prototypes::{
+        ProtoBinaryBinaryLweSeededKeyswitchKey32, ProtoBinaryBinaryLweSeededKeyswitchKey64,
+    };
+    use crate::generation::synthesizing::SynthesizesLweSeededKeyswitchKey;
+    use crate::generation::{Maker, Precision32, Precision64};
+    use concrete_core::prelude::{
+        DestructionEngine, LweSeededKeyswitchKey32, LweSeededKeyswitchKey64,
+    };
+
+    impl SynthesizesLweSeededKeyswitchKey<Precision32, LweSeededKeyswitchKey32> for Maker {
+        fn synthesize_lwe_seeded_keyswitch_key(
+            &mut self,
+            prototype: &Self::LweSeededKeyswitchKeyProto,
+        ) -> LweSeededKeyswitchKey32 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_lwe_seeded_keyswitch_key(
+            &mut self,
+            entity: LweSeededKeyswitchKey32,
+        ) -> Self::LweSeededKeyswitchKeyProto {
+            ProtoBinaryBinaryLweSeededKeyswitchKey32(entity)
+        }
+
+        fn destroy_lwe_seeded_keyswitch_key(&mut self, entity: LweSeededKeyswitchKey32) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+
+    impl SynthesizesLweSeededKeyswitchKey<Precision64, LweSeededKeyswitchKey64> for Maker {
+        fn synthesize_lwe_seeded_keyswitch_key(
+            &mut self,
+            prototype: &Self::LweSeededKeyswitchKeyProto,
+        ) -> LweSeededKeyswitchKey64 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_lwe_seeded_keyswitch_key(
+            &mut self,
+            entity: LweSeededKeyswitchKey64,
+        ) -> Self::LweSeededKeyswitchKeyProto {
+            ProtoBinaryBinaryLweSeededKeyswitchKey64(entity)
+        }
+
+        fn destroy_lwe_seeded_keyswitch_key(&mut self, entity: LweSeededKeyswitchKey64) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+}
