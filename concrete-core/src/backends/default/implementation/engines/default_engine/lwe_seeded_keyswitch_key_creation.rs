245b82e54dcc94c67d8538cbe627e27d1647d879
Arthur Meyre <arthur.meyre@zama.ai>
2022-06-24 17:23:07 +0200

    feat(core): add LweSeededKeyswitchKeyCreationEngine
    - add spec
    - add implementations

---
+++ concrete-core/src/backends/default/implementation/engines/default_engine/lwe_seeded_keyswitch_key_creation.rs
@@ -0,0 +1,224 @@
+use super::ActivatedRandomGenerator;
+use concrete_commons::dispersion::Variance;
+use concrete_commons::parameters::{DecompositionBaseLog, DecompositionLevelCount};
+
+use crate::backends::default::implementation::engines::DefaultEngine;
+use crate::backends::default::implementation::entities::{
+    LweSecretKey32, LweSecretKey64, LweSeededKeyswitchKey32, LweSeededKeyswitchKey64,
+};
+use crate::commons::crypto::lwe::LweSeededKeyswitchKey as ImplLweSeededKeyswitchKey;
+use crate::commons::math::random::{CompressionSeed, Seeder};
+use crate::specification::engines::{
+    LweSeededKeyswitchKeyCreationEngine, LweSeededKeyswitchKeyCreationError,
+};
+use crate::specification::entities::LweSecretKeyEntity;
+
+impl LweSeededKeyswitchKeyCreationEngine<LweSecretKey32, LweSecretKey32, LweSeededKeyswitchKey32>
+    for DefaultEngine
+{
+    /// # Example:
+    /// ```
+    /// use concrete_commons::dispersion::Variance;
+    /// use concrete_commons::parameters::{
+    ///     DecompositionBaseLog, DecompositionLevelCount, LweDimension,
+    /// };
+    /// use concrete_core::prelude::*;
+    /// # use std::error::Error;
+    ///
+    /// # fn main() -> Result<(), Box<dyn Error>> {
+    /// // DISCLAIMER: the parameters used here are only for test purpose, and are not secure.
+    /// let input_lwe_dimension = LweDimension(6);
+    /// let output_lwe_dimension = LweDimension(3);
+    /// let decomposition_level_count = DecompositionLevelCount(2);
+    /// let decomposition_base_log = DecompositionBaseLog(8);
+    /// let noise = Variance(2_f64.powf(-25.));
+    ///
+    /// // Unix seeder must be given a secret input.
+    /// // Here we just give it 0, which is totally unsafe.
+    /// const UNSAFE_SECRET: u128 = 0;
+    /// let mut engine = DefaultEngine::new(Box::new(UnixSeeder::new(UNSAFE_SECRET)))?;
+    /// let input_key: LweSecretKey32 = engine.create_lwe_secret_key(input_lwe_dimension)?;
+    /// let output_key: LweSecretKey32 = engine.create_lwe_secret_key(output_lwe_dimension)?;
+    ///
+    /// let seeded_keyswitch_key = engine.create_lwe_seeded_keyswitch_key(
+    ///     &input_key,
+    ///     &output_key,
+    ///     decomposition_level_count,
+    ///     decomposition_base_log,
+    ///     noise,
+    /// )?;
+    /// #
+    /// assert_eq!(
+    /// #     seeded_keyswitch_key.decomposition_level_count(),
+    /// #     decomposition_level_count
+    /// # );
+    /// assert_eq!(
+    /// #     seeded_keyswitch_key.decomposition_base_log(),
+    /// #     decomposition_base_log
+    /// # );
+    /// assert_eq!(seeded_keyswitch_key.input_lwe_dimension(), input_lwe_dimension);
+    /// assert_eq!(seeded_keyswitch_key.output_lwe_dimension(), output_lwe_dimension);
+    ///
+    /// engine.destroy(input_key)?;
+    /// engine.destroy(output_key)?;
+    /// engine.destroy(seeded_keyswitch_key)?;
+    /// #
+    /// # Ok(())
+    /// # }
+    /// ```
+    fn create_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &LweSecretKey32,
+        output_key: &LweSecretKey32,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Result<LweSeededKeyswitchKey32, LweSeededKeyswitchKeyCreationError<Self::EngineError>>
+    {
+        LweSeededKeyswitchKeyCreationError::perform_generic_checks(
+            decomposition_level_count,
+            decomposition_base_log,
+            32,
+        )?;
+        Ok(unsafe {
+            self.create_lwe_seeded_keyswitch_key_unchecked(
+                input_key,
+                output_key,
+                decomposition_level_count,
+                decomposition_base_log,
+                noise,
+            )
+        })
+    }
+
+    unsafe fn create_lwe_seeded_keyswitch_key_unchecked(
+        &mut self,
+        input_key: &LweSecretKey32,
+        output_key: &LweSecretKey32,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> LweSeededKeyswitchKey32 {
+        let mut ksk = ImplLweSeededKeyswitchKey::allocate(
+            decomposition_level_count,
+            decomposition_base_log,
+            input_key.lwe_dimension(),
+            output_key.lwe_dimension(),
+            CompressionSeed {
+                seed: self.seeder.seed(),
+            },
+        );
+        ksk.fill_with_seeded_keyswitch_key::<_, _, _, _, _, ActivatedRandomGenerator>(
+            &input_key.0,
+            &output_key.0,
+            noise,
+            &mut self.seeder,
+        );
+        LweSeededKeyswitchKey32(ksk)
+    }
+}
+
+impl LweSeededKeyswitchKeyCreationEngine<LweSecretKey64, LweSecretKey64, LweSeededKeyswitchKey64>
+    for DefaultEngine
+{
+    /// # Example:
+    /// ```
+    /// use concrete_commons::dispersion::Variance;
+    /// use concrete_commons::parameters::{
+    ///     DecompositionBaseLog, DecompositionLevelCount, LweDimension,
+    /// };
+    /// use concrete_core::prelude::*;
+    /// # use std::error::Error;
+    ///
+    /// # fn main() -> Result<(), Box<dyn Error>> {
+    /// // DISCLAIMER: the parameters used here are only for test purpose, and are not secure.
+    /// let input_lwe_dimension = LweDimension(6);
+    /// let output_lwe_dimension = LweDimension(3);
+    /// let decomposition_level_count = DecompositionLevelCount(2);
+    /// let decomposition_base_log = DecompositionBaseLog(8);
+    /// let noise = Variance(2_f64.powf(-25.));
+    ///
+    /// // Unix seeder must be given a secret input.
+    /// // Here we just give it 0, which is totally unsafe.
+    /// const UNSAFE_SECRET: u128 = 0;
+    /// let mut engine = DefaultEngine::new(Box::new(UnixSeeder::new(UNSAFE_SECRET)))?;
+    /// let input_key: LweSecretKey64 = engine.create_lwe_secret_key(input_lwe_dimension)?;
+    /// let output_key: LweSecretKey64 = engine.create_lwe_secret_key(output_lwe_dimension)?;
+    ///
+    /// let seeded_keyswitch_key = engine.create_lwe_seeded_keyswitch_key(
+    ///     &input_key,
+    ///     &output_key,
+    ///     decomposition_level_count,
+    ///     decomposition_base_log,
+    ///     noise,
+    /// )?;
+    /// #
+    /// assert_eq!(
+    /// #     seeded_keyswitch_key.decomposition_level_count(),
+    /// #     decomposition_level_count
+    /// # );
+    /// assert_eq!(
+    /// #     seeded_keyswitch_key.decomposition_base_log(),
+    /// #     decomposition_base_log
+    /// # );
+    /// assert_eq!(seeded_keyswitch_key.input_lwe_dimension(), input_lwe_dimension);
+    /// assert_eq!(seeded_keyswitch_key.output_lwe_dimension(), output_lwe_dimension);
+    ///
+    /// engine.destroy(input_key)?;
+    /// engine.destroy(output_key)?;
+    /// engine.destroy(seeded_keyswitch_key)?;
+    /// #
+    /// # Ok(())
+    /// # }
+    /// ```
+    fn create_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &LweSecretKey64,
+        output_key: &LweSecretKey64,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Result<LweSeededKeyswitchKey64, LweSeededKeyswitchKeyCreationError<Self::EngineError>>
+    {
+        LweSeededKeyswitchKeyCreationError::perform_generic_checks(
+            decomposition_level_count,
+            decomposition_base_log,
+            64,
+        )?;
+        Ok(unsafe {
+            self.create_lwe_seeded_keyswitch_key_unchecked(
+                input_key,
+                output_key,
+                decomposition_level_count,
+                decomposition_base_log,
+                noise,
+            )
+        })
+    }
+
+    unsafe fn create_lwe_seeded_keyswitch_key_unchecked(
+        &mut self,
+        input_key: &LweSecretKey64,
+        output_key: &LweSecretKey64,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> LweSeededKeyswitchKey64 {
+        let mut ksk = ImplLweSeededKeyswitchKey::allocate(
+            decomposition_level_count,
+            decomposition_base_log,
+            input_key.lwe_dimension(),
+            output_key.lwe_dimension(),
+            CompressionSeed {
+                seed: self.seeder.seed(),
+            },
+        );
+        ksk.fill_with_seeded_keyswitch_key::<_, _, _, _, _, ActivatedRandomGenerator>(
+            &input_key.0,
+            &output_key.0,
+            noise,
+            &mut self.seeder,
+        );
+        LweSeededKeyswitchKey64(ksk)
+    }
+}
---
+++ concrete-core/src/specification/engines/lwe_seeded_keyswitch_key_creation.rs
@@ -0,0 +1,83 @@
+use super::engine_error;
+use crate::specification::engines::AbstractEngine;
+
+use crate::specification::entities::{LweSecretKeyEntity, LweSeededKeyswitchKeyEntity};
+use concrete_commons::dispersion::Variance;
+use concrete_commons::parameters::{DecompositionBaseLog, DecompositionLevelCount};
+
+engine_error! {
+    LweSeededKeyswitchKeyCreationError for LweSeededKeyswitchKeyCreationEngine @
+    NullDecompositionBaseLog => "The key decomposition base log must be greater than zero.",
+    NullDecompositionLevelCount => "The key decomposition level count must be greater than zero.",
+    DecompositionTooLarge => "The decomposition precision (base log * level count) must not exceed \
+                              the precision of the ciphertext."
+}
+
+impl<EngineError: std::error::Error> LweSeededKeyswitchKeyCreationError<EngineError> {
+    /// Validates the inputs
+    pub fn perform_generic_checks(
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        integer_precision: usize,
+    ) -> Result<(), Self> {
+        if decomposition_base_log.0 == 0 {
+            return Err(Self::NullDecompositionBaseLog);
+        }
+
+        if decomposition_level_count.0 == 0 {
+            return Err(Self::NullDecompositionLevelCount);
+        }
+
+        if decomposition_level_count.0 * decomposition_base_log.0 > integer_precision {
+            return Err(Self::DecompositionTooLarge);
+        }
+
+        Ok(())
+    }
+}
+
+/// A trait for engines creating seeded LWE keyswitch keys.
+///
+/// # Semantics
+///
+/// This [pure](super#operation-semantics) operation creates a seeded LWE keyswitch key allowing to
+/// switch from the `input_key` LWE secret key to the `output_key` LWE secret key.
+///
+/// # Formal Definition
+///
+/// cf [`here`](`crate::specification::entities::LweSeededKeyswitchKeyEntity`)
+pub trait LweSeededKeyswitchKeyCreationEngine<InputSecretKey, OutputSecretKey, SeededKeyswitchKey>:
+    AbstractEngine
+where
+    InputSecretKey: LweSecretKeyEntity,
+    OutputSecretKey: LweSecretKeyEntity,
+    SeededKeyswitchKey: LweSeededKeyswitchKeyEntity<
+        InputKeyDistribution = InputSecretKey::KeyDistribution,
+        OutputKeyDistribution = OutputSecretKey::KeyDistribution,
+    >,
+{
+    /// Creates a seeded LWE keyswitch key.
+    fn create_lwe_seeded_keyswitch_key(
+        &mut self,
+        input_key: &InputSecretKey,
+        output_key: &OutputSecretKey,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> Result<SeededKeyswitchKey, LweSeededKeyswitchKeyCreationError<Self::EngineError>>;
+
+    /// Unsafely creates a seeded LWE keyswitch key.
+    ///
+    /// # Safety
+    /// For the _general_ safety concerns regarding this operation, refer to the different variants
+    /// of [`LweSeededKeyswitchKeyCreationError`]. For safety concerns _specific_ to an
+    /// engine, refer to the implementer safety section.
+    unsafe fn create_lwe_seeded_keyswitch_key_unchecked(
+        &mut self,
+        input_key: &InputSecretKey,
+        output_key: &OutputSecretKey,
+        decomposition_level_count: DecompositionLevelCount,
+        decomposition_base_log: DecompositionBaseLog,
+        noise: Variance,
+    ) -> SeededKeyswitchKey;
+}
