2c4704986c0be7e6aa52967907b286b0576a75ee
Arthur Meyre <arthur.meyre@zama.ai>
2022-06-27 09:51:40 +0200

    feat(fixture): add prototyping utilities for GlweSeededCiphertext

---
+++ concrete-core-fixture/src/generation/prototyping/glwe_seeded_ciphertext.rs
@@ -0,0 +1,94 @@
+use crate::generation::prototypes::{
+    GlweSeededCiphertextPrototype, ProtoBinaryGlweCiphertext32, ProtoBinaryGlweCiphertext64,
+    ProtoBinaryGlweSeededCiphertext32, ProtoBinaryGlweSeededCiphertext64,
+};
+use crate::generation::prototyping::glwe_ciphertext::PrototypesGlweCiphertext;
+use crate::generation::prototyping::glwe_secret_key::PrototypesGlweSecretKey;
+use crate::generation::prototyping::plaintext_vector::PrototypesPlaintextVector;
+use crate::generation::{IntegerPrecision, Maker, Precision32, Precision64};
+use concrete_commons::dispersion::Variance;
+use concrete_core::prelude::markers::{BinaryKeyDistribution, KeyDistributionMarker};
+use concrete_core::prelude::{
+    GlweSeededCiphertextEncryptionEngine, GlweSeededCiphertextToGlweCiphertextTransformationEngine,
+};
+
+/// A trait allowing to manipulate seeded GLWE ciphertext prototypes.
+pub trait PrototypesGlweSeededCiphertext<
+    Precision: IntegerPrecision,
+    KeyDistribution: KeyDistributionMarker,
+>:
+    PrototypesPlaintextVector<Precision>
+    + PrototypesGlweSecretKey<Precision, KeyDistribution>
+    + PrototypesGlweCiphertext<Precision, KeyDistribution>
+{
+    type GlweSeededCiphertextProto: GlweSeededCiphertextPrototype<
+        Precision = Precision,
+        KeyDistribution = KeyDistribution,
+    >;
+    fn encrypt_plaintext_vector_to_glwe_seeded_ciphertext(
+        &mut self,
+        secret_key: &Self::GlweSecretKeyProto,
+        plaintext_vector: &Self::PlaintextVectorProto,
+        noise: Variance,
+    ) -> Self::GlweSeededCiphertextProto;
+    fn transform_glwe_seeded_ciphertext_to_glwe_ciphertext(
+        &mut self,
+        seeded_ciphertext: &Self::GlweSeededCiphertextProto,
+    ) -> Self::GlweCiphertextProto;
+}
+
+impl PrototypesGlweSeededCiphertext<Precision32, BinaryKeyDistribution> for Maker {
+    type GlweSeededCiphertextProto = ProtoBinaryGlweSeededCiphertext32;
+
+    fn encrypt_plaintext_vector_to_glwe_seeded_ciphertext(
+        &mut self,
+        secret_key: &Self::GlweSecretKeyProto,
+        plaintext_vector: &Self::PlaintextVectorProto,
+        noise: Variance,
+    ) -> Self::GlweSeededCiphertextProto {
+        ProtoBinaryGlweSeededCiphertext32(
+            self.default_engine
+                .encrypt_glwe_seeded_ciphertext(&secret_key.0, &plaintext_vector.0, noise)
+                .unwrap(),
+        )
+    }
+
+    fn transform_glwe_seeded_ciphertext_to_glwe_ciphertext(
+        &mut self,
+        seeded_ciphertext: &Self::GlweSeededCiphertextProto,
+    ) -> ProtoBinaryGlweCiphertext32 {
+        ProtoBinaryGlweCiphertext32(
+            self.default_engine
+                .transform_glwe_seeded_ciphertext_to_glwe_ciphertext(seeded_ciphertext.0.to_owned())
+                .unwrap(),
+        )
+    }
+}
+
+impl PrototypesGlweSeededCiphertext<Precision64, BinaryKeyDistribution> for Maker {
+    type GlweSeededCiphertextProto = ProtoBinaryGlweSeededCiphertext64;
+
+    fn encrypt_plaintext_vector_to_glwe_seeded_ciphertext(
+        &mut self,
+        secret_key: &Self::GlweSecretKeyProto,
+        plaintext_vector: &Self::PlaintextVectorProto,
+        noise: Variance,
+    ) -> Self::GlweSeededCiphertextProto {
+        ProtoBinaryGlweSeededCiphertext64(
+            self.default_engine
+                .encrypt_glwe_seeded_ciphertext(&secret_key.0, &plaintext_vector.0, noise)
+                .unwrap(),
+        )
+    }
+
+    fn transform_glwe_seeded_ciphertext_to_glwe_ciphertext(
+        &mut self,
+        seeded_ciphertext: &Self::GlweSeededCiphertextProto,
+    ) -> ProtoBinaryGlweCiphertext64 {
+        ProtoBinaryGlweCiphertext64(
+            self.default_engine
+                .transform_glwe_seeded_ciphertext_to_glwe_ciphertext(seeded_ciphertext.0.to_owned())
+                .unwrap(),
+        )
+    }
+}
---
+++ concrete-core-fixture/src/generation/synthesizing/glwe_seeded_ciphertext.rs
@@ -0,0 +1,71 @@
+use crate::generation::prototyping::PrototypesGlweSeededCiphertext;
+use crate::generation::IntegerPrecision;
+use concrete_core::prelude::GlweSeededCiphertextEntity;
+
+/// A trait allowing to synthesize an actual GlweSeededCiphertextEntity from a prototype.
+pub trait SynthesizesGlweSeededCiphertext<Precision: IntegerPrecision, GlweSeededCiphertext>:
+    PrototypesGlweSeededCiphertext<Precision, GlweSeededCiphertext::KeyDistribution>
+where
+    GlweSeededCiphertext: GlweSeededCiphertextEntity,
+{
+    fn synthesize_glwe_seeded_ciphertext(
+        &mut self,
+        prototype: &Self::GlweSeededCiphertextProto,
+    ) -> GlweSeededCiphertext;
+    fn unsynthesize_glwe_seeded_ciphertext(
+        &mut self,
+        entity: GlweSeededCiphertext,
+    ) -> Self::GlweSeededCiphertextProto;
+    fn destroy_glwe_seeded_ciphertext(&mut self, entity: GlweSeededCiphertext);
+}
+
+mod backend_default {
+    use crate::generation::prototypes::{
+        ProtoBinaryGlweSeededCiphertext32, ProtoBinaryGlweSeededCiphertext64,
+    };
+    use crate::generation::synthesizing::SynthesizesGlweSeededCiphertext;
+    use crate::generation::{Maker, Precision32, Precision64};
+    use concrete_core::prelude::{
+        DestructionEngine, GlweSeededCiphertext32, GlweSeededCiphertext64,
+    };
+
+    impl SynthesizesGlweSeededCiphertext<Precision32, GlweSeededCiphertext32> for Maker {
+        fn synthesize_glwe_seeded_ciphertext(
+            &mut self,
+            prototype: &Self::GlweSeededCiphertextProto,
+        ) -> GlweSeededCiphertext32 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_glwe_seeded_ciphertext(
+            &mut self,
+            entity: GlweSeededCiphertext32,
+        ) -> Self::GlweSeededCiphertextProto {
+            ProtoBinaryGlweSeededCiphertext32(entity)
+        }
+
+        fn destroy_glwe_seeded_ciphertext(&mut self, entity: GlweSeededCiphertext32) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+
+    impl SynthesizesGlweSeededCiphertext<Precision64, GlweSeededCiphertext64> for Maker {
+        fn synthesize_glwe_seeded_ciphertext(
+            &mut self,
+            prototype: &Self::GlweSeededCiphertextProto,
+        ) -> GlweSeededCiphertext64 {
+            prototype.0.to_owned()
+        }
+
+        fn unsynthesize_glwe_seeded_ciphertext(
+            &mut self,
+            entity: GlweSeededCiphertext64,
+        ) -> Self::GlweSeededCiphertextProto {
+            ProtoBinaryGlweSeededCiphertext64(entity)
+        }
+
+        fn destroy_glwe_seeded_ciphertext(&mut self, entity: GlweSeededCiphertext64) {
+            self.default_engine.destroy(entity).unwrap();
+        }
+    }
+}
