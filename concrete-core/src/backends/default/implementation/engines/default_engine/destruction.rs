14d7527f430d76bd2a29ba2f7da2a1912797ae25
Arthur Meyre <arthur.meyre@zama.ai>
2022-06-27 15:22:52 +0200

    feat(core): add specification and impls for GgswSeededCiphertextEntity

--- concrete-core/src/backends/default/implementation/engines/default_engine/destruction.rs
+++ concrete-core/src/backends/default/implementation/engines/default_engine/destruction.rs
@@ -1,17 +1,17 @@
 use crate::backends::default::implementation::engines::DefaultEngine;
 use crate::backends::default::implementation::entities::{
     Cleartext32, Cleartext64, CleartextVector32, CleartextVector64, GgswCiphertext32,
-    GgswCiphertext64, GlweCiphertext32, GlweCiphertext64, GlweCiphertextMutView32,
-    GlweCiphertextMutView64, GlweCiphertextVector32, GlweCiphertextVector64, GlweCiphertextView32,
-    GlweCiphertextView64, GlweSecretKey32, GlweSecretKey64, GlweSeededCiphertext32,
-    GlweSeededCiphertext64, GlweSeededCiphertextVector32, GlweSeededCiphertextVector64,
-    LweBootstrapKey32, LweBootstrapKey64, LweCiphertext32, LweCiphertext64, LweCiphertextMutView32,
-    LweCiphertextMutView64, LweCiphertextVector32, LweCiphertextVector64, LweCiphertextView32,
-    LweCiphertextView64, LweKeyswitchKey32, LweKeyswitchKey64, LweSecretKey32, LweSecretKey64,
-    LweSeededCiphertext32, LweSeededCiphertext64, LweSeededCiphertextVector32,
-    LweSeededCiphertextVector64, LweSeededKeyswitchKey32, LweSeededKeyswitchKey64,
-    PackingKeyswitchKey32, PackingKeyswitchKey64, Plaintext32, Plaintext64, PlaintextVector32,
-    PlaintextVector64,
+    GgswCiphertext64, GgswSeededCiphertext32, GgswSeededCiphertext64, GlweCiphertext32,
+    GlweCiphertext64, GlweCiphertextMutView32, GlweCiphertextMutView64, GlweCiphertextVector32,
+    GlweCiphertextVector64, GlweCiphertextView32, GlweCiphertextView64, GlweSecretKey32,
+    GlweSecretKey64, GlweSeededCiphertext32, GlweSeededCiphertext64, GlweSeededCiphertextVector32,
+    GlweSeededCiphertextVector64, LweBootstrapKey32, LweBootstrapKey64, LweCiphertext32,
+    LweCiphertext64, LweCiphertextMutView32, LweCiphertextMutView64, LweCiphertextVector32,
+    LweCiphertextVector64, LweCiphertextView32, LweCiphertextView64, LweKeyswitchKey32,
+    LweKeyswitchKey64, LweSecretKey32, LweSecretKey64, LweSeededCiphertext32,
+    LweSeededCiphertext64, LweSeededCiphertextVector32, LweSeededCiphertextVector64,
+    LweSeededKeyswitchKey32, LweSeededKeyswitchKey64, PackingKeyswitchKey32, PackingKeyswitchKey64,
+    Plaintext32, Plaintext64, PlaintextVector32, PlaintextVector64,
 };
 use crate::commons::math::tensor::AsMutTensor;
 use crate::prelude::{CleartextF64, CleartextVectorF64, FloatEncoder, FloatEncoderVector};
@@ -399,6 +399,30 @@ impl DestructionEngine<GgswCiphertext64> for DefaultEngine {
     }
 
     unsafe fn destroy_unchecked(&mut self, _entity: &mut GgswCiphertext64) {}
+}
+
+impl DestructionEngine<GgswSeededCiphertext32> for DefaultEngine {
+    fn destroy(
+        &mut self,
+        mut entity: GgswSeededCiphertext32,
+    ) -> Result<(), DestructionError<Self::EngineError>> {
+        unsafe { self.destroy_unchecked(&mut entity) };
+        Ok(())
+    }
+
+    unsafe fn destroy_unchecked(&mut self, _entity: &mut GgswSeededCiphertext32) {}
+}
+
+impl DestructionEngine<GgswSeededCiphertext64> for DefaultEngine {
+    fn destroy(
+        &mut self,
+        mut entity: GgswSeededCiphertext64,
+    ) -> Result<(), DestructionError<Self::EngineError>> {
+        unsafe { self.destroy_unchecked(&mut entity) };
+        Ok(())
+    }
+
+    unsafe fn destroy_unchecked(&mut self, _entity: &mut GgswSeededCiphertext64) {}
 }
 
 impl DestructionEngine<LweBootstrapKey32> for DefaultEngine {
