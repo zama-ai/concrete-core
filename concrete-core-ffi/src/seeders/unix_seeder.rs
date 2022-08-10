c5251f303628d77aa1d7778fb6c80a360ef15045
Arthur Meyre <arthur.meyre@zama.ai>
2022-08-10 10:53:23 +0200

    chore(ffi): do not limit creations of UnixSeeder from a single secret
    - we were being overly cautious, a secret here is like a secret key

--- concrete-core-ffi/src/seeders/unix_seeder.rs
+++ concrete-core-ffi/src/seeders/unix_seeder.rs
@@ -7,29 +7,16 @@ use std::os::raw::c_int;
 
 struct UnixSeederFactory {
     secret: u128,
-    allowed_to_create_seeder: bool,
 }
 
 impl UnixSeederFactory {
     pub fn new(secret: u128) -> Self {
-        Self {
-            secret,
-            allowed_to_create_seeder: true,
-        }
+        Self { secret }
     }
 }
 
 impl SeederFactory for UnixSeederFactory {
     fn create_seeder(&mut self) -> Result<Box<dyn Seeder>, String> {
-        if !self.allowed_to_create_seeder {
-            return Err(
-                "The current UnixSeederFactory already created a seeder with the stored secret \
-                and will not create anymore for security reasons. If you need a new UnixSeeder \
-                create another UnixSeederFactory with a different secret."
-                    .to_owned(),
-            );
-        }
-        self.allowed_to_create_seeder = false;
         Ok(Box::new(UnixSeeder::new(self.secret)))
     }
 }
@@ -60,15 +47,12 @@ fn u128_from_two_u64(high_64: u64, low_64: u64) -> u128 {
     (high_bytes << 64) ^ low_bytes
 }
 
-/// ⚠ It is VERY dangerous to re-use a secret to seed a UnixSeeder. The returned [`SeederBuilder`]
-/// instance is only callable once and will panic if called more than once.
-///
 /// This function requires a 128 bits seed that will be used as a secret to create a `UnixSeeder`
 /// instance. Two 64 bits unsigned integers (`secret_high_64` and `secret_low_64`) are required to
 /// create the 128 bits secret.
 ///
-/// Return a [`SeederBuilder`] which will create only one `UnixSeeder`, that can be passed to engine
-/// creation functions that require it. Using the [`SeederBuilder`] more than once will panic.
+/// Return a [`SeederBuilder`] which yields `UnixSeeder`s that can be passed to engine creation
+/// functions that require it.
 ///
 /// This function is [checked](crate#safety-checked-and-unchecked-functions).
 #[no_mangle]
@@ -86,9 +70,6 @@ pub unsafe extern "C" fn get_unix_seeder_builder(
     })
 }
 
-/// ⚠ It is VERY dangerous to re-use a secret to seed a UnixSeeder. The returned [`SeederBuilder`]
-/// instance is only callable once and will panic if called more than once.
-///
 /// [Unchecked](crate#safety-checked-and-unchecked-functions) version of
 /// [`get_unix_seeder_builder`].
 #[no_mangle]
