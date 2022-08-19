65ee8cb0b2e4ca0df54bec0965069e0ed0c7cc99
Arthur Meyre <arthur.meyre@zama.ai>
2022-08-19 14:57:50 +0200

    feat(core): add LweBootstrapKey (Mut)View32/64

--- concrete-core/src/backends/default/implementation/entities/lwe_bootstrap_key.rs
+++ concrete-core/src/backends/default/implementation/entities/lwe_bootstrap_key.rs
@@ -78,3 +78,143 @@ pub(crate) enum LweBootstrapKey64Version {
     #[serde(other)]
     Unsupported,
 }
+
+/// A structure representing an LWE bootstrap key with 32 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but mutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Mutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweBootstrapKeyMutView32<'a>(pub(crate) ImplStandardBootstrapKey<&'a mut [u32]>);
+impl AbstractEntity for LweBootstrapKeyMutView32<'_> {
+    type Kind = LweBootstrapKeyKind;
+}
+impl LweBootstrapKeyEntity for LweBootstrapKeyMutView32<'_> {
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.glwe_size().to_glwe_dimension()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+
+    fn input_lwe_dimension(&self) -> LweDimension {
+        self.0.key_size()
+    }
+
+    fn decomposition_base_log(&self) -> DecompositionBaseLog {
+        self.0.base_log()
+    }
+
+    fn decomposition_level_count(&self) -> DecompositionLevelCount {
+        self.0.level_count()
+    }
+}
+
+/// A structure representing an LWE bootstrap key with 64 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but mutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Mutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweBootstrapKeyMutView64<'a>(pub(crate) ImplStandardBootstrapKey<&'a mut [u64]>);
+impl AbstractEntity for LweBootstrapKeyMutView64<'_> {
+    type Kind = LweBootstrapKeyKind;
+}
+impl LweBootstrapKeyEntity for LweBootstrapKeyMutView64<'_> {
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.glwe_size().to_glwe_dimension()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+
+    fn input_lwe_dimension(&self) -> LweDimension {
+        self.0.key_size()
+    }
+
+    fn decomposition_base_log(&self) -> DecompositionBaseLog {
+        self.0.base_log()
+    }
+
+    fn decomposition_level_count(&self) -> DecompositionLevelCount {
+        self.0.level_count()
+    }
+}
+
+/// A structure representing an LWE bootstrap key with 32 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but immutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Immutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweBootstrapKeyView32<'a>(pub(crate) ImplStandardBootstrapKey<&'a [u32]>);
+impl AbstractEntity for LweBootstrapKeyView32<'_> {
+    type Kind = LweBootstrapKeyKind;
+}
+impl LweBootstrapKeyEntity for LweBootstrapKeyView32<'_> {
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.glwe_size().to_glwe_dimension()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+
+    fn input_lwe_dimension(&self) -> LweDimension {
+        self.0.key_size()
+    }
+
+    fn decomposition_base_log(&self) -> DecompositionBaseLog {
+        self.0.base_log()
+    }
+
+    fn decomposition_level_count(&self) -> DecompositionLevelCount {
+        self.0.level_count()
+    }
+}
+
+/// A structure representing an LWE bootstrap key with 64 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but immutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Immutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweBootstrapKeyView64<'a>(pub(crate) ImplStandardBootstrapKey<&'a [u64]>);
+impl AbstractEntity for LweBootstrapKeyView64<'_> {
+    type Kind = LweBootstrapKeyKind;
+}
+impl LweBootstrapKeyEntity for LweBootstrapKeyView64<'_> {
+    fn glwe_dimension(&self) -> GlweDimension {
+        self.0.glwe_size().to_glwe_dimension()
+    }
+
+    fn polynomial_size(&self) -> PolynomialSize {
+        self.0.polynomial_size()
+    }
+
+    fn input_lwe_dimension(&self) -> LweDimension {
+        self.0.key_size()
+    }
+
+    fn decomposition_base_log(&self) -> DecompositionBaseLog {
+        self.0.base_log()
+    }
+
+    fn decomposition_level_count(&self) -> DecompositionLevelCount {
+        self.0.level_count()
+    }
+}
