ef1257962a216f133b4457c9830005cd97de63d2
aPere3 <alexandre.pere@zama.ai>
2022-08-23 16:49:33 +0200

    feat(concrete_core_wasm): automatize api generation

---
+++ concrete-core-wasm/build/build.rs
@@ -0,0 +1,70 @@
+use concrete_core_representation::load_ccr;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+pub mod generation;
+pub mod prune;
+
+fn main() -> Result<(), String> {
+    // We load the neighbouring ccr
+    let mut ccr = load_ccr(get_concrete_core_root());
+
+    // We prune the ccir from unneeded nodes
+    prune::prune(&mut ccr);
+
+    // We generate the binding
+    let binding = generation::generate_binding(&ccr).to_string();
+
+    // We format the binding with rustfmt
+    let formatted_binding = format_binding(binding)?;
+
+    // We write the binding to a `__gen.rs` file
+    write_gen_rs(&formatted_binding);
+
+    Ok(())
+}
+
+fn write_gen_rs(binding: &str) {
+    let path = out_dir().join("__gen.rs");
+    let mut file = File::create(&path).unwrap();
+    file.write_all(binding.as_bytes()).unwrap();
+}
+
+fn out_dir() -> PathBuf {
+    PathBuf::from(env::var("OUT_DIR").unwrap())
+}
+
+fn get_concrete_core_root() -> PathBuf {
+    PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("..")
+        .canonicalize()
+        .unwrap()
+        .join("concrete-core/src/lib.rs")
+}
+
+fn format_binding(input: String) -> Result<String, String> {
+    let mut rustfmt = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+    let mut stdin = rustfmt
+        .stdin
+        .take()
+        .ok_or_else(|| "Failed to take stdin".to_string())?;
+    std::thread::spawn(move || {
+        stdin
+            .write_all(input.as_bytes())
+            .expect("failed to write to stdin");
+    });
+    let output = rustfmt
+        .wait_with_output()
+        .map_err(|e| format!("Failed to gather rustfmt output: {}", e))?;
+    if !output.status.success() {
+        return Err("Failed to format binding.".to_string());
+    }
+    String::from_utf8(output.stdout).map_err(|e| format!("Failed to read rustfmt output: {}", e))
+}
---
+++ concrete-core-wasm/build/generation.rs
@@ -0,0 +1,500 @@
+//! This module contains a function that generates the binding token stream from the pruned ccr.
+use concrete_core_representation::{ConcreteCore, *};
+use quote::{format_ident, quote, ToTokens};
+use syn::__private::{Span, TokenStream2};
+use syn::{parse_quote, Ident, Type};
+
+/// Generate the whole binding from the pruned ccr.
+pub fn generate_binding(ccr: &ConcreteCore) -> TokenStream2 {
+    let mut output = TokenStream2::new();
+    output.extend(generate_all_entities(ccr));
+    output.extend(generate_all_configs(ccr));
+    output.extend(generate_all_engines(ccr));
+    output
+}
+
+fn generate_all_entities(ccr: &ConcreteCore) -> TokenStream2 {
+    let mut output = TokenStream2::new();
+    for entity in ccr
+        .backends
+        .iter()
+        .flat_map(|backend| backend.entities.iter())
+    {
+        output.extend(generate_entity_definition(entity).into_iter());
+    }
+    output
+}
+
+fn generate_entity_definition(entity: &Entity) -> TokenStream2 {
+    // INVARIANT: entities are available in the `concrete_core::prelude` module
+    // INVARIANT: views are not exported
+    let ident = entity.definition.item_struct.ident.clone();
+    quote! {
+        #[wasm_bindgen]
+        pub struct #ident(pub(crate) concrete_core::prelude::#ident);
+    }
+}
+
+fn generate_all_configs(ccr: &ConcreteCore) -> TokenStream2 {
+    let mut output = TokenStream2::new();
+    for config in ccr
+        .backends
+        .iter()
+        .flat_map(|backend| backend.configs.iter())
+    {
+        output.extend(generate_config_definition(config).into_iter());
+        output.extend(generate_config_constructor(config).into_iter());
+        output.extend(generate_config_converter(config).into_iter());
+    }
+    output
+}
+
+fn generate_config_definition(config: &Config) -> TokenStream2 {
+    let config_ident = &config.item_struct.ident;
+    let fields = &config
+        .item_struct
+        .fields
+        .iter()
+        .map(|field| {
+            let ident = field.ident.clone().unwrap();
+            let type_ = field.ty.clone();
+            quote!(pub #ident: #type_)
+        })
+        .collect::<Vec<_>>();
+    quote! {
+        #[wasm_bindgen]
+        #[derive(Serialize, Deserialize)]
+        pub struct #config_ident{
+            #(#fields),*
+        }
+    }
+}
+
+fn generate_config_constructor(config: &Config) -> TokenStream2 {
+    let config_ident = &config.item_struct.ident;
+    let args = &config
+        .item_struct
+        .fields
+        .iter()
+        .map(|field| {
+            let ident = field.ident.clone().unwrap();
+            let type_ = field.ty.clone();
+            quote!(#ident: #type_)
+        })
+        .collect::<Vec<_>>();
+    let fields = &config
+        .item_struct
+        .fields
+        .iter()
+        .map(|field| {
+            let ident = field.ident.clone().unwrap();
+            quote!(#ident)
+        })
+        .collect::<Vec<_>>();
+    quote! {
+        #[wasm_bindgen]
+        impl #config_ident{
+            #[wasm_bindgen(constructor)]
+            pub fn new(#(#args),*) -> #config_ident {
+                #config_ident{
+                    #(#fields),*
+                }
+            }
+        }
+    }
+}
+
+fn generate_config_converter(config: &Config) -> TokenStream2 {
+    let config_ident = &config.item_struct.ident;
+    let fields = &config
+        .item_struct
+        .fields
+        .iter()
+        .map(|field| {
+            let ident = field.ident.clone().unwrap();
+            quote!(#ident)
+        })
+        .collect::<Vec<_>>();
+    quote! {
+        impl #config_ident{
+            fn to_concrete_core_type(&self) -> concrete_core::prelude::#config_ident {
+                concrete_core::prelude::#config_ident{
+                    #(#fields: self.#fields),*
+                }
+            }
+        }
+    }
+}
+
+fn generate_all_engines(ccr: &ConcreteCore) -> TokenStream2 {
+    let mut output = TokenStream2::new();
+    for engine in ccr
+        .backends
+        .iter()
+        .flat_map(|backend| backend.engines.iter())
+    {
+        output.extend(generate_engine_definition(engine));
+        output.extend(generate_engine_constructor(engine));
+        for engine_impl in engine.engine_impls.iter() {
+            output.extend(generate_engine_method(engine_impl));
+        }
+    }
+    output
+}
+
+fn generate_engine_definition(engine: &Engine) -> TokenStream2 {
+    // INVARIANT: engines are available in the `concrete_core::prelude` module
+    let ident = engine.definition.item_struct.ident.clone();
+    quote! {
+        #[wasm_bindgen]
+        pub struct #ident(pub(crate) concrete_core::prelude::#ident);
+    }
+}
+
+fn generate_engine_constructor(engine: &Engine) -> TokenStream2 {
+    // INVARIANT: the constructor parameter is either a path or a tuple of path.
+    // INVARIANT: one constructor parameter may be a `Box<dyn Seeder>`
+    // INVARIANT: type paths in the constructor parameters only contain a single value.
+    let engine_ident = engine.definition.get_name();
+    let public_constructor_args = generate_public_constructor_args(
+        engine.abstract_engine_impl.get_parameters_associated_type(),
+    );
+    let private_constructor_exprs = generate_private_constructor_exprs(
+        engine.abstract_engine_impl.get_parameters_associated_type(),
+    );
+    quote! {
+        #[wasm_bindgen]
+        impl #engine_ident{
+            #[wasm_bindgen(constructor)]
+            pub fn new(#public_constructor_args) -> Result<#engine_ident, JsError> {
+                std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+                concrete_core::prelude::#engine_ident::new(#private_constructor_exprs)
+                    .map_err(|e| wasm_bindgen::JsError::new(format!("{}", e).as_str()))
+                    .map(#engine_ident)
+            }
+        }
+    }
+}
+
+fn generate_public_constructor_args(parameter_associated_type: &Type) -> TokenStream2 {
+    // INVARIANT: The parameter associated type is either a path or a tuple of paths.
+
+    // If the parameter is a `Box<dyn Seeder>`
+    let test_ast: syn::TypePath = parse_quote!(Box<dyn Seeder>);
+    let maybe_seeder = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Path(p) => p,
+        p ?> *p == &test_ast
+    );
+    if let Some(_seeder) = maybe_seeder {
+        return quote!(seeder: crate::JsFunctionSeeder);
+    }
+
+    // If the parameter is a path.
+    let maybe_path = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Path(p) => p
+    );
+    if let Some(path) = maybe_path {
+        return quote!(arg: #path);
+    }
+
+    // If the parameter is a tuple of paths
+    let maybe_tuple_of_paths = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Tuple(t) => t,
+        t ?> t.elems.iter().all(|elm| matches!(elm, syn::Type::Path(_)))
+    );
+    if let Some(tuple) = maybe_tuple_of_paths {
+        let mut output = TokenStream2::new();
+        for (i, elm) in tuple.elems.iter().enumerate() {
+            if let syn::Type::Path(p) = elm {
+                let ident = format_ident!("input_{}", i);
+                output.extend(quote!(#ident: #p));
+            } else {
+                unreachable!()
+            }
+        }
+        return output;
+    }
+
+    panic!("Failed to generate public constructor args.");
+}
+
+fn generate_private_constructor_exprs(parameter_associated_type: &Type) -> TokenStream2 {
+    // INVARIANT: The parameter associated type is either a path or a tuple of paths.
+
+    // If the parameter is a `Box<dyn Seeder>`
+    let test_ast: syn::TypePath = parse_quote!(Box<dyn Seeder>);
+    let maybe_seeder = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Path(p) => p,
+        p ?> *p == &test_ast
+    );
+    if let Some(_seeder) = maybe_seeder {
+        return quote!(Box::new(seeder));
+    }
+
+    // If the parameter is a path.
+    let maybe_path = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Path(p) => p
+    );
+    if let Some(_path) = maybe_path {
+        return quote!(arg);
+    }
+
+    // If the parameter is a tuple of paths
+    let maybe_tuple_of_paths = probe!(
+        Some(parameter_associated_type),
+        syn::Type::Tuple(t) => t,
+        t ?> t.elems.iter().all(|elm| matches!(elm, syn::Type::Path(_)))
+    );
+    if let Some(tuple) = maybe_tuple_of_paths {
+        let mut output = TokenStream2::new();
+        for (i, elm) in tuple.elems.iter().enumerate() {
+            if let syn::Type::Path(_p) = elm {
+                let ident = format_ident!("input_{}", i);
+                output.extend(quote!(#ident,));
+            } else {
+                unreachable!()
+            }
+        }
+        return quote!((#output));
+    }
+
+    panic!("Failed to generate public constructor args.");
+}
+
+fn generate_engine_method(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    let engine_ident = &engine_impl.engine_type_ident;
+    let method_ident = generate_engine_method_ident(engine_impl);
+    let method_args = generate_engine_method_args(engine_impl);
+    let method_return = generate_engine_method_return_type(engine_impl);
+    let method_body = generate_engine_method_body(engine_impl);
+    let method_return_map = generate_engine_method_return_map(engine_impl);
+    quote! {
+        #[wasm_bindgen]
+        impl #engine_ident {
+            pub fn #method_ident(&mut self, #method_args) -> Result<#method_return, JsError>{
+                #method_body
+                    .map_err(|e| wasm_bindgen::JsError::new(format!("{}", e).as_str()))
+                    #method_return_map
+            }
+        }
+    }
+}
+
+fn generate_engine_method_return_map(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    match engine_impl.checked_method.return_() {
+        EngineTraitImplReturn::OwnedEntity(v) => {
+            quote!(.map(#v))
+        }
+        EngineTraitImplReturn::Numeric(_) => {
+            quote!()
+        }
+        EngineTraitImplReturn::NumericVec(_) => {
+            quote!()
+        }
+        EngineTraitImplReturn::Unit(_) => {
+            quote!()
+        }
+        _ => panic!(
+            "Unexpected return type: {:?}",
+            engine_impl.checked_method.return_()
+        ),
+    }
+}
+
+fn generate_engine_method_body(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    let mut blocks = TokenStream2::new();
+    engine_impl.checked_method.args().iter().for_each(|arg| {
+        if let EngineTraitImplArg::ConfigSlice(pat, _, config_ident) = arg {
+            let block = quote! {
+                let #pat = #pat
+                    .iter()
+                    .map(|jsval| serde_wasm_bindgen::from_value(jsval.to_owned()))
+                    .collect::<Result<Vec<#config_ident>, _>>()
+                    .map_err(|e| wasm_bindgen::JsError::new(format!("{}", e).as_str()))?;
+                let #pat = #pat.into_iter().map(|t| t.to_concrete_core_type()).collect::<Vec<_>>();
+            };
+            blocks.extend(block)
+        }
+    });
+
+    let mut args = TokenStream2::new();
+    engine_impl
+        .checked_method
+        .args()
+        .iter()
+        .for_each(|arg| match arg {
+            EngineTraitImplArg::OwnedEntity(pat, _) => args.extend(quote!(#pat.0,)),
+            EngineTraitImplArg::OwnedEntityRef(pat, _) => args.extend(quote!(& #pat.0,)),
+            EngineTraitImplArg::OwnedEntityRefMut(pat, _) => args.extend(quote!(&mut #pat.0,)),
+            EngineTraitImplArg::Config(pat, _) => {
+                args.extend(quote!(#pat.to_concrete_core_type(),))
+            }
+            EngineTraitImplArg::ConfigRef(pat, _, _) => {
+                args.extend(quote!(& #pat.to_concrete_core_type(),))
+            }
+            EngineTraitImplArg::ConfigSlice(pat, _, _) => args.extend(quote!(
+                #pat.as_slice(),
+            )),
+            EngineTraitImplArg::Parameter(pat, _) => args.extend(quote!(#pat.0,)),
+            EngineTraitImplArg::Dispersion(pat, _) => args.extend(quote!(#pat.0,)),
+            EngineTraitImplArg::Numeric(pat, _) => args.extend(quote!(#pat,)),
+            EngineTraitImplArg::NumericRef(pat, _, _) => args.extend(quote!(& #pat,)),
+            EngineTraitImplArg::NumericSlice(pat, _, _) => args.extend(quote!(#pat,)),
+            EngineTraitImplArg::NumericSliceMut(pat, _, _) => args.extend(quote!(#pat,)),
+            EngineTraitImplArg::NumericVec(pat, _, _) => args.extend(quote!(#pat,)),
+            _ => {
+                panic!("Unexpected arg: {:?}", arg)
+            }
+        });
+    let method_name = &engine_impl.checked_method.ident;
+    quote!(
+        #blocks
+        self.0.#method_name(#args)
+    )
+}
+
+fn generate_engine_method_return_type(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    let return_type = engine_impl.checked_method.return_().type_();
+    quote!(#return_type)
+}
+
+fn generate_engine_method_args(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    let mut output = TokenStream2::new();
+    engine_impl
+        .checked_method
+        .args()
+        .iter()
+        .for_each(|arg| match arg {
+            EngineTraitImplArg::NumericRef(pat_ident, _, num_ident) => {
+                output.extend(quote! {#pat_ident: #num_ident,});
+            }
+            EngineTraitImplArg::ConfigSlice(pat_ident, _, _) => {
+                output.extend(quote! {#pat_ident: Box<[JsValue]>,});
+            }
+            arg => {
+                let pat_ident = arg.pat_ident();
+                let type_ = arg.type_();
+                output.extend(quote! {#pat_ident: #type_,});
+            }
+        });
+    output
+}
+
+fn generate_engine_method_ident(engine_impl: &EngineTraitImpl) -> TokenStream2 {
+    let output = format!(
+        "{}_{}",
+        engine_impl.checked_method.to_fragment(),
+        engine_impl
+            .engine_trait_parameters()
+            .iter()
+            .map(ToNameFragment::to_fragment)
+            .collect::<Vec<_>>()
+            .join("_")
+    );
+    Ident::new(output.as_str(), Span::call_site()).to_token_stream()
+}
+
+trait ToNameFragment {
+    fn to_fragment(&self) -> String;
+}
+
+impl ToNameFragment for EngineTraitImplCheckedMethod {
+    fn to_fragment(&self) -> String {
+        self.ident.to_string()
+    }
+}
+
+impl ToNameFragment for EngineTraitImplGenericArgument {
+    fn to_fragment(&self) -> String {
+        match self {
+            EngineTraitImplGenericArgument::OwnedEntity(_)
+            | EngineTraitImplGenericArgument::ViewEntity(_)
+            | EngineTraitImplGenericArgument::MutViewEntity(_)
+            | EngineTraitImplGenericArgument::Config(_) => probe!(
+                Some(self.get_type()),
+                syn::Type::Path(t) => t,
+                t >> t.path.segments.first(),
+                t -> t.ident.to_string(),
+                t -> camel_case_to_snake_case(&t)
+            )
+            .unwrap(),
+            EngineTraitImplGenericArgument::Numeric(t) => probe!(
+                Some(t),
+                syn::Type::Path(t) => t,
+                t >> t.path.segments.first(),
+                t -> t.ident.to_string()
+            )
+            .unwrap(),
+            EngineTraitImplGenericArgument::NumericSlice(t) => probe!(
+                Some(t),
+                syn::Type::Reference(r) => r,
+                r -> r.elem.as_ref(),
+                syn::Type::Slice(s) => s,
+                s -> s.elem.as_ref(),
+                syn::Type::Path(p) => p,
+                t >> t.path.segments.first(),
+                t -> format!("{}_slice", t.ident)
+            )
+            .unwrap(),
+            EngineTraitImplGenericArgument::NumericSliceMut(t) => probe!(
+                Some(t),
+                syn::Type::Reference(r) => r,
+                r -> r.elem.as_ref(),
+                syn::Type::Slice(s) => s,
+                s -> s.elem.as_ref(),
+                syn::Type::Path(p) => p,
+                t >> t.path.segments.first(),
+                t -> format!("{}_mut_slice", t.ident)
+            )
+            .unwrap(),
+            EngineTraitImplGenericArgument::NumericVec(t) => probe!(
+                Some(t),
+                syn::Type::Path(p) => p,
+                p >> p.path.segments.first(),
+                s -> &s.arguments,
+                syn::PathArguments::AngleBracketed(a) => a,
+                a >> a.args.first(),
+                syn::GenericArgument::Type(t) => t,
+                syn::Type::Path(p) => p,
+                t >> t.path.segments.first(),
+                t -> format!("{}_vec", t.ident)
+            )
+            .unwrap(),
+            EngineTraitImplGenericArgument::Unknown(_) => {
+                panic!()
+            }
+        }
+    }
+}
+
+fn camel_case_to_snake_case(input: &str) -> String {
+    let mut output = String::new();
+    let mut iter = input.chars().peekable();
+    loop {
+        match (iter.next(), iter.peek()) {
+            (Some(ch), _) if !ch.is_ascii_alphanumeric() => {
+                panic!()
+            }
+            (Some(ch), Some(nch)) if ch.is_ascii_uppercase() && nch.is_ascii_lowercase() => {
+                output.push(ch.to_ascii_lowercase())
+            }
+            (Some(ch), Some(nch)) if ch.is_ascii_uppercase() && nch.is_ascii_uppercase() => {
+                output.push(ch.to_ascii_lowercase());
+                output.push('_')
+            }
+            (Some(ch), Some(nch)) if nch.is_ascii_uppercase() => {
+                output.push(ch);
+                output.push('_')
+            }
+            (Some(ch), _) => output.push(ch.to_ascii_lowercase()),
+            (None, None) => break,
+            _ => panic!("Failed to transform case of `{}`", input),
+        }
+    }
+    output
+}
