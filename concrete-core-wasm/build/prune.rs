c1ff681e0c0bbe3afe6b021753692e984a7c0646
aPere3 <alexandre.pere@zama.ai>
2022-08-23 16:49:33 +0200

    feat(concrete_core_wasm): automatize api generation

---
+++ concrete-core-wasm/build/prune.rs
@@ -0,0 +1,79 @@
+//! This module contains a function that prunes the ccr to only keep the elements that we want to
+//! expose in the wasm api.
+use concrete_core_representation::{
+    ConcreteCore, EngineTraitImplArg, EngineTraitImplReturn, EntityOwnership,
+};
+
+const BLACKLISTED_BACKENDS: [&str; 2] = ["cuda", "fftw"];
+
+/// A function that prunes a concrete-core representation from the nodes we do not want in the wasm
+/// api
+pub fn prune(ccr: &mut ConcreteCore) {
+    prune_blacklisted_backends(ccr);
+    prune_unavailable_entities(ccr);
+    prune_unavailable_engine_impls(ccr);
+}
+
+/// Prunes the blacklisted backends from the ccr
+fn prune_blacklisted_backends(ccr: &mut ConcreteCore) {
+    ccr.backends.retain(|backend| {
+        !BLACKLISTED_BACKENDS
+            .iter()
+            .any(|blacklisted| backend.ident == *blacklisted)
+    });
+}
+
+/// Prunes the unavailable entities from the ccr (views and mut views)
+fn prune_unavailable_entities(ccr: &mut ConcreteCore) {
+    for backend in ccr.backends.iter_mut() {
+        backend
+            .entities
+            .retain(|entity| matches!(entity.definition.ownership, EntityOwnership::Owned))
+    }
+}
+
+/// Prunes the engine trait impls that take unavailable arguments.
+fn prune_unavailable_engine_impls(ccr: &mut ConcreteCore) {
+    for engine in ccr
+        .backends
+        .iter_mut()
+        .flat_map(|backend| backend.engines.iter_mut())
+    {
+        engine.engine_impls.retain(|impl_| {
+            let args_available = impl_.checked_method.args().iter().all(|arg| match arg {
+                EngineTraitImplArg::OwnedEntity(_, _) => true,
+                EngineTraitImplArg::OwnedEntityRef(_, _) => true,
+                EngineTraitImplArg::OwnedEntityRefMut(_, _) => true,
+                EngineTraitImplArg::ViewEntity(_, _) => false,
+                EngineTraitImplArg::ViewEntityRef(_, _) => false,
+                EngineTraitImplArg::MutViewEntity(_, _) => false,
+                EngineTraitImplArg::MutViewEntityRefMut(_, _) => false,
+                EngineTraitImplArg::Config(_, _) => true,
+                EngineTraitImplArg::ConfigRef(_, _, _) => true,
+                EngineTraitImplArg::ConfigSlice(_, _, _) => true,
+                EngineTraitImplArg::Parameter(_, _) => true,
+                EngineTraitImplArg::Dispersion(_, _) => true,
+                EngineTraitImplArg::Numeric(_, _) => true,
+                EngineTraitImplArg::NumericRef(_, _, _) => true,
+                EngineTraitImplArg::NumericRefMut(_, _, _) => false,
+                EngineTraitImplArg::NumericSlice(_, _, _) => true,
+                EngineTraitImplArg::NumericSliceMut(_, _, _) => false,
+                EngineTraitImplArg::NumericVec(_, _, _) => true,
+                EngineTraitImplArg::Unknown(_, _) => false,
+            });
+            let return_available = match impl_.checked_method.return_() {
+                EngineTraitImplReturn::OwnedEntity(_) => true,
+                EngineTraitImplReturn::ViewEntity(_) => false,
+                EngineTraitImplReturn::MutViewEntity(_) => false,
+                EngineTraitImplReturn::Config(_) => false,
+                EngineTraitImplReturn::Numeric(_) => true,
+                EngineTraitImplReturn::NumericSlice(_) => false,
+                EngineTraitImplReturn::NumericSliceMut(_) => false,
+                EngineTraitImplReturn::NumericVec(_) => true,
+                EngineTraitImplReturn::Unit(_) => true,
+                EngineTraitImplReturn::Unknown(_) => false,
+            };
+            args_available && return_available
+        });
+    }
+}
--- concrete-core-wasm/src/lib.rs
+++ concrete-core-wasm/src/lib.rs
@@ -1,13 +1,90 @@
-mod engines;
-pub use engines::*;
+#![allow(clippy::boxed_local)]
+use concrete_core::specification::engines::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
 
-mod entities;
-pub use entities::*;
+include!(concat!(env!("OUT_DIR"), "/__gen.rs"));
 
-mod seeder;
+mod seeder {
+    use concrete_core::commons::math::random::Seed;
+    use concrete_core::prelude::Seeder;
+    use js_sys::{Function, Uint8Array};
+    use std::panic;
+    use wasm_bindgen::prelude::*;
+    use wasm_bindgen::JsValue;
+
+    const SEED_BYTES_COUNT: usize = 16;
+
+    #[wasm_bindgen]
+    pub struct JsFunctionSeeder {
+        js_func: Function,
+        buffer: [u8; SEED_BYTES_COUNT],
+    }
+
+    #[wasm_bindgen]
+    impl JsFunctionSeeder {
+        #[wasm_bindgen(constructor)]
+        pub fn new(js_func: Function) -> JsFunctionSeeder {
+            panic::set_hook(Box::new(console_error_panic_hook::hook));
+            let buffer = [0u8; SEED_BYTES_COUNT];
+            JsFunctionSeeder { js_func, buffer }
+        }
+    }
+
+    impl Seeder for JsFunctionSeeder {
+        fn seed(&mut self) -> Seed {
+            let output = self.js_func.call0(&JsValue::NULL).unwrap();
+            let array = Uint8Array::new(&output);
+            if array.length() as usize != SEED_BYTES_COUNT {
+                panic!("The seeder function must return a Uint8Array of size 16.");
+            }
+            array.copy_to(&mut self.buffer);
+            let seed = u128::from_le_bytes(self.buffer);
+            Seed(seed)
+        }
+
+        fn is_available() -> bool
+        where
+            Self: Sized,
+        {
+            true
+        }
+    }
+}
 pub use seeder::*;
 
-mod commons;
-pub use commons::*;
+mod commons {
+    use wasm_bindgen::prelude::*;
 
-type JsResult<Val> = Result<Val, wasm_bindgen::JsError>;
+    macro_rules! param {
+    ($(($public: ident, $private: ident, $typ: ty)),*) => {
+        $(
+            #[wasm_bindgen]
+            pub struct $public(pub(crate) concrete_core::prelude::$private);
+
+            #[wasm_bindgen]
+            impl $public {
+                #[wasm_bindgen(constructor)]
+                pub fn new(val: $typ) -> $public {
+                    $public(concrete_core::prelude::$private(val))
+                }
+            }
+        )*
+    };
+}
+
+    param! {
+        (Variance, Variance, f64),
+        (DecompositionBaseLog, DecompositionBaseLog, usize),
+        (DecompositionLevelCount, DecompositionLevelCount, usize),
+        (LweDimension, LweDimension, usize),
+        (LweCiphertextCount, LweCiphertextCount, usize),
+        (LweSize, LweSize, usize),
+        (MonomialIndex, MonomialIndex, usize),
+        (GlweDimension, GlweDimension, usize),
+        (GlweSize, GlweSize, usize),
+        (GlweCiphertextCount, GlweCiphertextCount, usize),
+        (PolynomialSize, PolynomialSize, usize)
+    }
+}
+pub use commons::*;
