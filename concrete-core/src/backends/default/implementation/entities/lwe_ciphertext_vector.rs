ecfd54c483880ef8c871627d90a1f213d0910326
Pedro Alves <pedro.alves@zama.ai>
2022-08-31 11:14:52 +0200

    feat(core): add views for LweCiphertextVector entities in the default backend

--- concrete-core/src/backends/default/implementation/entities/lwe_ciphertext_vector.rs
+++ concrete-core/src/backends/default/implementation/entities/lwe_ciphertext_vector.rs
@@ -56,3 +56,106 @@ pub(crate) enum LweCiphertextVector64Version {
     #[serde(other)]
     Unsupported,
 }
+
+// LweCiphertextVectorViews are just LweCiphertextVector entities that do not own their memory, 
+// they use a slice as a container as opposed to Vec for the standard LweCiphertextVector
+
+/// A structure representing a vector of LWE ciphertext views, with 32 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but immutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Immutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweCiphertextVectorView32<'a>(pub(crate) ImplLweList<&'a [u32]>);
+
+impl AbstractEntity for LweCiphertextVectorView32<'_> {
+    type Kind = LweCiphertextVectorKind;
+}
+
+impl LweCiphertextVectorEntity for LweCiphertextVectorView32<'_> {
+    fn lwe_dimension(&self) -> LweDimension {
+        self.0.lwe_size().to_lwe_dimension()
+    }
+
+    fn lwe_ciphertext_count(&self) -> LweCiphertextCount {
+        LweCiphertextCount(self.0.count().0)
+    }
+}
+
+/// A structure representing a vector of LWE ciphertext views, with 32 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but mutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Mutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweCiphertextVectorMutView32<'a>(pub(crate) ImplLweList<&'a mut [u32]>);
+
+impl AbstractEntity for LweCiphertextVectorMutView32<'_> {
+    type Kind = LweCiphertextVectorKind;
+}
+
+impl LweCiphertextVectorEntity for LweCiphertextVectorMutView32<'_> {
+    fn lwe_dimension(&self) -> LweDimension {
+        self.0.lwe_size().to_lwe_dimension()
+    }
+
+    fn lwe_ciphertext_count(&self) -> LweCiphertextCount {
+        LweCiphertextCount(self.0.count().0)
+    }
+}
+
+/// A structure representing a vector of LWE ciphertext views, with 64 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but immutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Immutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweCiphertextVectorView64<'a>(pub(crate) ImplLweList<&'a [u64]>);
+
+impl AbstractEntity for LweCiphertextVectorView64<'_> {
+    type Kind = LweCiphertextVectorKind;
+}
+
+impl LweCiphertextVectorEntity for LweCiphertextVectorView64<'_> {
+    fn lwe_dimension(&self) -> LweDimension {
+        self.0.lwe_size().to_lwe_dimension()
+    }
+
+    fn lwe_ciphertext_count(&self) -> LweCiphertextCount {
+        LweCiphertextCount(self.0.count().0)
+    }
+}
+
+/// A structure representing a vector of LWE ciphertext views, with 64 bits of precision.
+///
+/// By _view_ here, we mean that the entity does not own the data, but mutably borrows it.
+///
+/// Notes:
+/// ------
+/// This view is not Clone as Clone for a slice is not defined. It is not Deserialize either,
+/// as Deserialize of a slice is not defined. Mutable variant.
+#[derive(Debug, PartialEq, Eq)]
+pub struct LweCiphertextVectorMutView64<'a>(pub(crate) ImplLweList<&'a mut [u64]>);
+
+impl AbstractEntity for LweCiphertextVectorMutView64<'_> {
+    type Kind = LweCiphertextVectorKind;
+}
+
+impl LweCiphertextVectorEntity for LweCiphertextVectorMutView64<'_> {
+    fn lwe_dimension(&self) -> LweDimension {
+        self.0.lwe_size().to_lwe_dimension()
+    }
+
+    fn lwe_ciphertext_count(&self) -> LweCiphertextCount {
+        LweCiphertextCount(self.0.count().0)
+    }
+}
