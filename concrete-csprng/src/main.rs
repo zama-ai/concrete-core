8e72bd75b4d47725ab4f6526a1e1bd15574059a4
Arthur Meyre <arthur.meyre@zama.ai>
2022-09-21 15:56:22 +0200

    feat(csprng): implement AppleSecureEnclaveSeeder
    - re-use work from this original PR:
    https://github.com/zama-ai/concrete-core/pull/4

--- concrete-csprng/src/main.rs
+++ concrete-csprng/src/main.rs
@@ -1,13 +1,27 @@
 //! This program uses the concrete csprng to generate an infinite stream of random bytes on
 //! the program stdout. For testing purpose.
-use concrete_csprng::generators::{AesniRandomGenerator, RandomGenerator};
-use concrete_csprng::seeders::{RdseedSeeder, Seeder};
+#[cfg(target_arch = "x86_64")]
+use concrete_csprng::generators::AesniRandomGenerator as ActivatedRandomGenerator;
+#[cfg(target_arch = "aarch64")]
+use concrete_csprng::generators::ArmAesRandomGenerator as ActivatedRandomGenerator;
+#[cfg(all(not(target_arch = "x86_64"), not(target_arch = "aarch64")))]
+use concrete_csprng::generators::SoftwareRandomGenerator as ActivatedRandomGenerator;
+
+use concrete_csprng::generators::RandomGenerator;
+
+#[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+use concrete_csprng::seeders::AppleSecureEnclaveSeeder as ActivatedSeeder;
+#[cfg(target_arch = "x86_64")]
+use concrete_csprng::seeders::RdseedSeeder as ActivatedSeeder;
+
+use concrete_csprng::seeders::Seeder;
+
 use std::io::prelude::*;
 use std::io::stdout;
 
 pub fn main() {
-    let mut seeder = RdseedSeeder;
-    let mut generator = AesniRandomGenerator::new(seeder.seed());
+    let mut seeder = ActivatedSeeder;
+    let mut generator = ActivatedRandomGenerator::new(seeder.seed());
     let mut stdout = stdout();
     let mut buffer = [0u8; 16];
     loop {
--- concrete-csprng/src/seeders/implem/mod.rs
+++ concrete-csprng/src/seeders/implem/mod.rs
@@ -1,3 +1,8 @@
+#[cfg(target_os = "macos")]
+mod apple_secure_enclave_seeder;
+#[cfg(target_os = "macos")]
+pub use apple_secure_enclave_seeder::AppleSecureEnclaveSeeder;
+
 #[cfg(feature = "seeder_x86_64_rdseed")]
 mod rdseed;
 #[cfg(feature = "seeder_x86_64_rdseed")]
