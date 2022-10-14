c469dde889502c0a4e6e3a0e87c50da51ed7788c
Agnes Leroy <agnes.leroy@zama.ai>
2022-10-14 11:57:43 +0200

    chore(cuda): format sources and add check in ci

--- concrete-tasks/src/chore.rs
+++ concrete-tasks/src/chore.rs
@@ -3,7 +3,14 @@ use crate::{cmd, format_latex_doc};
 use std::io::Error;
 
 pub fn format() -> Result<(), Error> {
-    cmd!(&format!("cargo {} fmt", get_nightly_toolchain()?))
+    cmd!(&format!(
+        "cargo {} fmt &&
+                  clang-format -i concrete-cuda/cuda/include/* &&
+                  clang-format -i concrete-cuda/cuda/src/*.cu &&
+                  clang-format -i concrete-cuda/cuda/src/*.cuh &&
+                  clang-format -i concrete-cuda/cuda/src/*/*.*",
+        get_nightly_toolchain()?
+    ))
 }
 
 pub fn format_latex_doc() -> Result<(), Error> {
