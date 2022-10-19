03297dc21f999c3b484cc6b3a96f0802d1b37048
Agnes Leroy <agnes.leroy@zama.ai>
2022-10-19 12:03:11 +0200

    chore(csprng): fix dry run

--- concrete-csprng/src/main.rs
+++ concrete-csprng/src/main.rs
@@ -1,17 +1,27 @@
 //! This program uses the concrete csprng to generate an infinite stream of random bytes on
 //! the program stdout. For testing purpose.
-#[cfg(target_arch = "x86_64")]
+#[cfg(feature = "generator_x86_64_aesni")]
 use concrete_csprng::generators::AesniRandomGenerator as ActivatedRandomGenerator;
-#[cfg(target_arch = "aarch64")]
+#[cfg(feature = "generator_aarch64_aes")]
 use concrete_csprng::generators::NeonAesRandomGenerator as ActivatedRandomGenerator;
-#[cfg(all(not(target_arch = "x86_64"), not(target_arch = "aarch64")))]
+#[cfg(all(
+    not(feature = "generator_x86_64_aesni"),
+    not(feature = "generator_aarch64_aes"),
+    feature = "generator_soft"
+))]
 use concrete_csprng::generators::SoftwareRandomGenerator as ActivatedRandomGenerator;
 
 use concrete_csprng::generators::RandomGenerator;
 
-#[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+#[cfg(target_os = "macos")]
 use concrete_csprng::seeders::AppleSecureEnclaveSeeder as ActivatedSeeder;
-#[cfg(target_arch = "x86_64")]
+#[cfg(all(not(target_os = "macos"), feature = "seeder_x86_64_rdseed"))]
+use concrete_csprng::seeders::RdseedSeeder as ActivatedSeeder;
+#[cfg(all(
+    not(target_os = "macos"),
+    not(feature = "seeder_x86_64_rdseed"),
+    feature = "seeder_unix"
+))]
 use concrete_csprng::seeders::RdseedSeeder as ActivatedSeeder;
 
 use concrete_csprng::seeders::Seeder;
